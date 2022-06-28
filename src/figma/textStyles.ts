a0c3d72e1117caad6b64d0b6790ea18ed575a4d4
Ryan <rzimmerman@talusanalytics.com>
2022-06-28 17:16:39 -0300

    Adding text styles and color palette from Figma

--- src/figma/textStyles.ts
+++ src/figma/textStyles.ts
@@ -1,78 +1,26 @@
 export default {
-  textStyleH1: `
+	textStyleH1: `
 	font-size:36px;
-	font-family:"Rawline";
-	font-weight:bolder;
+	font-family:"Poppins";
+	font-weight:400;
 	font-style:normal;
 	text-decoration:none;
+	text-transform:none;
 `,
-  textStyleH2: `
-	font-size:30px;
-	font-family:"Rawline";
-	font-weight:bolder;
+	textStyleH2: `
+	font-size:28px;
+	font-family:"Poppins";
+	font-weight:400;
 	font-style:normal;
-	line-height:34px;
 	text-decoration:none;
+	text-transform:none;
 `,
-  textStyleH3: `
-	font-size:24px;
-	font-family:"Rawline";
-	font-weight:bolder;
-	font-style:normal;
-	line-height:30px;
-	text-decoration:none;
-`,
-  textStyleAlertTitles: `
-	font-size:20px;
-	font-family:"Open Sans", Arial, Helvetica, sans-serif;
-	font-weight:bolder;
-	font-style:normal;
-	line-height:25px;
-	text-decoration:none;
-`,
-  textStyleParagraph: `
-	font-size:16px;
-	font-family:"Open Sans", Arial, Helvetica, sans-serif;
-	font-weight:normal;
-	font-style:normal;
-	line-height:25px;
-	text-decoration:none;
-`,
-  textStyleParagraphSemibold: `
-	font-size:16px;
-	font-family:"Open Sans", Arial, Helvetica, sans-serif;
-	font-weight:bolder;
-	font-style:normal;
-	line-height:25px;
-	text-decoration:none;
-`,
-  textStyleParagraphBold: `
-	font-size:16px;
-	font-family:"Open Sans", Arial, Helvetica, sans-serif;
-	font-weight:bolder;
-	font-style:normal;
-	line-height:25px;
-	text-decoration:none;
-`,
-  textStyleSmallLabel: `
-	font-size:12px;
-	font-family:"Open Sans", Arial, Helvetica, sans-serif;
-	font-weight:normal;
+	textStyleBigParagraph: `
+	font-size:28px;
+	font-family:"Poppins";
+	font-weight:400;
 	font-style:normal;
 	text-decoration:none;
-`,
-  textStyleChartLabels: `
-	font-size:16px;
-	font-family:"Open Sans", Arial, Helvetica, sans-serif;
-	font-weight:normal;
-	font-style:normal;
-	text-decoration:none;
-`,
-  textStyleChartLabelsEmphasis: `
-	font-size:16px;
-	font-family:"Open Sans", Arial, Helvetica, sans-serif;
-	font-weight:bolder;
-	font-style:normal;
-	text-decoration:none;
+	text-transform:none;
 `,
 }
