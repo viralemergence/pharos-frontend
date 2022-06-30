ced8f0a2de25c771760cd1e4ba65cd427ed9d195
Ryan <rzimmerman@talusanalytics.com>
2022-06-30 19:27:35 -0300

    Placeholder view for dataset editor

---
+++ src/components/AuthenticatedViews/DatasetEditor/DatasetEditor.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Main from 'components/layout/Main'
+
+const DatasetEditor = () => (
+  <Main>
+    <h1>Dataset editor</h1>
+  </Main>
+)
+
+export default DatasetEditor
