5a44aaf4bf1e3265f22a0a74ed45509558d9e2b9
Ryan <rzimmerman@talusanalytics.com>
2022-06-30 17:03:43 -0300

    Updating providers to set up default user state and provide it to the app

--- src/components/layout/Providers.tsx
+++ src/components/layout/Providers.tsx
@@ -1,29 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 import CMS from '@talus-analytics/library.airtable-cms'
 
-// import textStyles from '../figma/textStyles'
-// import textStyles from '../../figma/textStyles'
+import useSiteMetadataQuery from 'cmsHooks/useSiteMetadataQuery'
+import UserContextProvider, { User, UserStatus } from './UserContext'
+import useIconsQuery from 'cmsHooks/useIconsQuery'
+import getTrackingId from 'utilities/trackingId'
+
 import textStyles from 'figma/textStyles'
 import colorPalette from 'figma/colorPalette'
-
-import useIconsQuery from 'cmsHooks/useIconsQuery'
-import useSiteMetadataQuery from 'cmsHooks/useSiteMetadataQuery'
-import getTrackingId from 'utilities/trackingId'
 
 import '../../../static/assets/fonts/fonts.css'
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  } 
   body {
     margin: 0;
     padding: 0;
     font-family: "Poppins", Arial, Helvetica, sans-serif;
     color: ${({ theme }) => theme.black};
-  }
-  * {
-    box-sizing: border-box;
   }
 `
 
@@ -34,13 +33,19 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
   // get GA tracking ID
   const trackingId = getTrackingId()
 
+  const [user, setUser] = useState<User>({
+    status: UserStatus.loggedOut,
+  })
+
   return (
     <CMS.IconProvider data={icons}>
       <CMS.SiteMetadataProvider data={siteMetadata} trackingId={trackingId}>
-        <ThemeProvider theme={{ ...textStyles, ...colorPalette }}>
-          <GlobalStyle />
-          {children}
-        </ThemeProvider>
+        <UserContextProvider data={[user, setUser]}>
+          <ThemeProvider theme={{ ...textStyles, ...colorPalette }}>
+            <GlobalStyle />
+            {children}
+          </ThemeProvider>
+        </UserContextProvider>
       </CMS.SiteMetadataProvider>
     </CMS.IconProvider>
   )
