d423256f4c0e7fed94d3c378b6aace5b005843c1
Ryan <rzimmerman@talusanalytics.com>
2022-06-30 17:04:11 -0300

    Simple function to authenticate the user

---
+++ src/components/Login/authenticate.ts
@@ -0,0 +1,26 @@
+import { User, UserStatus } from 'components/layout/UserContext'
+
+const authenticate = async (researcherID: string) => {
+  const response = await fetch(`${process.env.GATSBY_API_URL}/auth`, {
+    method: 'POST',
+    body: `{"researcherID":"${researcherID}"}`,
+  }).catch(error => console.log(error))
+
+  console.log(response)
+  if (!response) return { status: UserStatus.authError }
+
+  if (response.status == 500) return { status: UserStatus.invalidUser }
+
+  if (!response.ok) return { status: UserStatus.authError }
+
+  const data = await response.json()
+
+  const user = {
+    status: UserStatus.loggedIn,
+    data: data,
+  }
+
+  return user as User
+}
+
+export default authenticate
