55f8dc6145d2100cd47412f2b627592522558d24
Ryan <rzimmerman@talusanalytics.com>
2022-07-06 14:27:20 -0300

    Function to create datasets (this is mostly placeholder for error handling later)

---
+++ src/components/AuthenticatedViews/ResearcherHome/CreateDatasetForm/createDataset.ts
@@ -0,0 +1,21 @@
+const createDataset = async (
+  researcherID: string,
+  dataset_name: string,
+  samples_taken: number,
+  detection_run: number
+) => {
+  const response = await fetch(`${process.env.GATSBY_API_URL}/create-dataset`, {
+    method: 'POST',
+    body: JSON.stringify({
+      researcherID,
+      dataset_name,
+      samples_taken,
+      detection_run,
+    }),
+  }).catch(error => console.log(error))
+
+  if (!response || !response.ok) return false
+  return true
+}
+
+export default createDataset
