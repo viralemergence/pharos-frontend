6cb61850773dc0c3e6b740568ff36074107edc6d
Ryan <rzimmerman@talusanalytics.com>
2022-07-06 11:30:34 -0300

    Refactor app.tsx to replace ClientRouter.tsx

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,15 +1,41 @@
 import React from 'react'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 
 import CMS from '@talus-analytics/library.airtable-cms'
 import Providers from 'components/layout/Providers'
 
+import Login from 'components/Login/Login'
 import NavBar from 'components/layout/NavBar/NavBar'
-import ClientRouter from 'components/ClientRouter/ClientRouter'
+import RequireAuth from 'components/ClientRouter/RequireAuth'
+
+import DatasetEditor from 'components/AuthenticatedViews/DatasetEditor/DatasetEditor'
+import ResearcherHome from 'components/AuthenticatedViews/ResearcherHome/ResearcherHome'
 
 const App = (): JSX.Element => (
   <Providers>
     <CMS.SEO />
-    <ClientRouter />
+    <HashRouter>
+      <NavBar />
+      <Routes>
+        <Route path={'/login'} element={<Login />} />
+        <Route
+          path={'/dataset'}
+          element={
+            <RequireAuth>
+              <DatasetEditor />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path={'/'}
+          element={
+            <RequireAuth>
+              <ResearcherHome />
+            </RequireAuth>
+          }
+        />
+      </Routes>
+    </HashRouter>
   </Providers>
 )
 
