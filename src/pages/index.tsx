092bc13533435ac517c2ceabdaa246249e24bacf
Ryan <rzimmerman@talusanalytics.com>
2022-07-06 11:18:50 -0300

    The index page, the `/` route, should always be the logged out page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,16 @@ import React from 'react'
 import CMS from '@talus-analytics/library.airtable-cms'
 import Providers from 'components/layout/Providers'
 
+// import NavBar from 'components/layout/NavBar/NavBar'
+// import ClientRouter from 'components/ClientRouter/ClientRouter'
+import LoggedOutLanding from 'components/LoggedOutLanding/LoggedOutLanding'
 import NavBar from 'components/layout/NavBar/NavBar'
-import ClientRouter from 'components/ClientRouter/ClientRouter'
 
 const IndexPage = (): JSX.Element => (
   <Providers>
     <CMS.SEO />
     <NavBar />
-    <ClientRouter />
+    <LoggedOutLanding />
   </Providers>
 )
 
