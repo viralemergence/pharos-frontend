9f7c39bfd0a4e3b4978fe1eec1cae6413c2c9d51
Ryan <rzimmerman@talusanalytics.com>
2022-07-08 20:13:07 -0300

    Fix indentation, add label

--- src/components/AuthenticatedViews/DatasetEditor/VersionSwitcher/VersionSwitcher.tsx
+++ src/components/AuthenticatedViews/DatasetEditor/VersionSwitcher/VersionSwitcher.tsx
@@ -6,32 +6,35 @@ import useProject from 'hooks/useProject'
 import useDataset from 'hooks/useDatset'
 
 const VersionSwitcher = () => {
-	const { id } = useParams()
-	const [, projectDispatch] = useProject()
-	const dataset = useDataset(id)
+  const { id } = useParams()
+  const [, projectDispatch] = useProject()
+  const dataset = useDataset(id)
 
-	if (!id || !dataset || !dataset.versions) return <></>
+  if (!id || !dataset || !dataset.versions) return <></>
 
-	const onSelectVersion = (e: React.ChangeEvent<HTMLSelectElement>) => {
-		const nextVersion = e.target.value
+  const onSelectVersion = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const nextVersion = e.target.value
 
-		projectDispatch({
-			type: ProjectActions.SetActiveVersion,
-			payload: { datasetID: id, version: Number(nextVersion) },
-		})
-	}
+    projectDispatch({
+      type: ProjectActions.SetActiveVersion,
+      payload: { datasetID: id, version: Number(nextVersion) },
+    })
+  }
 
-	return (
-		<select
-			onChange={onSelectVersion}
-			value={dataset.activeVersion}
-			style={{ marginLeft: 'auto', marginRight: '15px', fontSize: 18 }}
-		>
-			{dataset.versions.map((version, index) => (
-				<option value={index}>{version.date}</option>
-			))}
-		</select>
-	)
+  return (
+    <label>
+      Select version:{' '}
+      <select
+        onChange={onSelectVersion}
+        value={dataset.activeVersion}
+        style={{ marginLeft: '1ex' }}
+      >
+        {dataset.versions.map((version, index) => (
+          <option value={index}>{version.date}</option>
+        ))}
+      </select>
+    </label>
+  )
 }
 
 export default VersionSwitcher
