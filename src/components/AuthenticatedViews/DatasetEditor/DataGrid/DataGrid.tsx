ca640db4f8e96128eb8af74f646996a2afe3830c
Ryan <rzimmerman@talusanalytics.com>
2022-07-14 16:58:45 -0300

    Finishing refactoring DatasetGrid to work with all the new stuff

--- src/components/AuthenticatedViews/DatasetEditor/DataGrid/DataGrid.tsx
+++ src/components/AuthenticatedViews/DatasetEditor/DataGrid/DataGrid.tsx
@@ -1,21 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
+import DataGrid from 'react-data-grid'
 import { useParams } from 'react-router-dom'
 
-import DataGrid from 'react-data-grid'
+import { RegisterStatus } from 'reducers/projectReducer/types'
 
-import TextEditor from './TextEditor/TextEditor'
+import TextEditor from './editors/TextEditor'
 
-import {
-  ProjectStatus,
-  Record,
-  RegisterStatus,
-} from 'reducers/projectReducer/types'
-
-import useProject from 'hooks/useProject'
-import { ProjectActions } from 'reducers/projectReducer/projectReducer'
-import useVersionRows from 'hooks/useVersionRows'
 import SimpleFormatter from './formatters/SimpleFormatter'
+
+import useVersionRows from 'hooks/useVersionRows'
+import useRegisterStatus from 'hooks/useRegisterStatus'
 
 const Container = styled.div`
   margin-top: 30px;
@@ -27,22 +22,13 @@ const FillDatasetGrid = styled(DataGrid)`
 `
 
 const DatasetGrid = () => {
-  const { id } = useParams()
-  const [project] = useProject()
+  const { id: datasetID } = useParams()
 
-  const versionRows = useVersionRows(id, 0)
+  const versionRows = useVersionRows(datasetID, 0)
+  const registerStatus = useRegisterStatus(datasetID)
 
-  if (project.status !== ProjectStatus.Loaded) return <></>
-
-  if (!id) throw new Error('Missing dataset ID url parameter')
-
-  const dataset = project.datasets[id]
-  if (!dataset) return <></>
-
-  if (!Object.keys(dataset.register)) return <p>No active version</p>
-
-  if (dataset.registerStatus === RegisterStatus.Loading)
-    return <p>Loading version</p>
+  if (registerStatus === RegisterStatus.Loading) return <p>Loading version</p>
+  if (!versionRows) return <p>No active version</p>
 
   const columns = Object.keys(versionRows).map(key => ({
     key,
@@ -51,58 +37,12 @@ const DatasetGrid = () => {
     formatter: SimpleFormatter,
   }))
 
-  // const handleChange = (rows: Record[]) => {
-  //   // check if this version is not saved
-  //   // if it's not saved, we just replace the
-  //   // current rows with the new rows
-
-  //   // if the edit uses up the last row, create a new row
-  //   // check if there are any cells in the last row which are not empty
-  //   if (
-  //     !Object.values(rows.slice(-1)[0]).every(cell => cell.displayValue == '')
-  //   )
-  //     // use the keys from the first row to create a new row where all
-  //     // cells are empty and add it to the end of a copy of the rows array
-  //     rows = [
-  //       ...rows,
-  //       Object.keys(rows[0]).reduce(
-  //         (acc, key) => ({
-  //           ...acc,
-  //           [key]: { displayValue: '', dataValue: '' },
-  //         }),
-  //         {}
-  //       ),
-  //     ]
-
-  //   if (version.status === VersionStatus.Unsaved)
-  //     projectDispatch({
-  //       type: ProjectActions.UpdateVersion,
-  //       payload: { datasetID: id, version: { rows: rows } },
-  //     })
-  //   // if the project was in saved state, we
-  //   // need to create a new version with a
-  //   // new timestamp and use these rows
-  //   else
-  //     projectDispatch({
-  //       type: ProjectActions.CreateVersion,
-  //       payload: {
-  //         datasetID: id,
-  //         version: {
-  //           date: new Date().toUTCString(),
-  //           status: VersionStatus.Unsaved,
-  //           rows,
-  //         },
-  //       },
-  //     })
-  // }
-
   return (
     <Container>
       <FillDatasetGrid
         className={'rdg-light'}
         columns={columns}
         rows={versionRows}
-        // onRowsChange={rows => handleChange(rows as Record[])}
         // rowKeyGetter={}
       />
     </Container>
