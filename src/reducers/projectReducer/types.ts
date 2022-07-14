80961e5a56d8690392f30d7c5ac97822d77cbb88
Ryan <rzimmerman@talusanalytics.com>
2022-07-14 12:08:23 -0300

    No changes; reordering file to make it easier to read

--- src/reducers/projectReducer/types.ts
+++ src/reducers/projectReducer/types.ts
@@ -1,96 +1,37 @@
-// the status states of the overall
-// datasets object
-export enum ProjectStatus {
-  Initial,
-  Loading,
-  Loaded,
-  NetworkError,
-}
+// this is the set of projects
+// the user has access to and
+// their state in the frontend
 
-// all possible statuses for a
-// dataset in the frontend
-export enum DatasetStatus {
-  Unsaved,
-  Saving,
-  Saved,
-  Error,
-}
-
-// all possible statuses for a register
-// relative to the server
-export enum RegisterStatus {
-  Unsaved,
-  Saving,
-  Saved,
-  Error,
-  Loading,
-}
-
-export enum ReportScore {
-  fail,
-  pass,
-  warning,
-}
-
-// a single component of a record; the user
-// sees this as a cell in the dataset table
-// or in a form-like interface
-export interface Datapoint {
-  // human readable label
-  displayValue: string | number
-  // value of the datapoint inside our system,
-  // where that needs to be different; for
-  // example to implement links to other data
-  // such as organisms or locations
-  dataValue: string
-  // validation report response
-  report?: {
-    pass: ReportScore
-    message: string
+// portfolio is not yet implemented
+export interface Portfolio {
+  status: PortfolioStatus
+  projects: {
+    [key: string]: Project
   }
-  // if this datapoint was modified
-  // in the active version
-  modified?: boolean
-  // userID
-  modifiedBy?: string
-  // version number where this
-  // datapoint was last modified.
-  // it is valid for any version
-  // higher than this number
-  version: number
-  // to access versions before
-  // the version number, traverse
-  // the linked list.
-  previous?: Datapoint
 }
 
-// a single record of data, a collection
-// of datapoints which the user sees
-// as a row in a table
-export interface Record {
-  [key: string]: Datapoint
-}
+// the overall Project object
+// representing everything the frontend
+// currently knows about each project
 
-// each Record in the register
-// is a collection of datapoints
-// which the user will see as a
-// row, or as an input form
-export interface Register {
-  [key: string]: Record
-}
-
-// this is the container for information
-// that globally applies to an entire version,
-// the index in the array is the corresponding
-// version number, and the length of the array
-// is the number of versions which should be
-// in the register.
-export interface Version {
-  // the creation date of the version
-  date: string
-  // name of version; adding this as an
-  // example of future metadata we might need
-  name: string
+// project is implemented in the frontend
+// but not in the API yet.
+export interface Project {
+  projectID: string
+  status: ProjectStatus // don't save in api
+  datasets: {
+    // dataset keys are the datasetID
+    // to allow O(1) access to update
+    // individual datasets in reducer
+    // functions.
+    [key: string]: Dataset
+  }
+  // adding this as a note
+  // organisms should be implemented
+  // as a special case of a dataset
+  // organisms: {
+  //   [key: string]: Dataset
+  // }
 }
 
 // a single dataset
@@ -130,31 +71,106 @@ export interface Dataset {
   date_collected?: string
 }
 
-// the overall Project object
-// representing everything the frontend
-// currently knows about every dataset
-export interface Project {
-  projectID: string
-  status: ProjectStatus // don't save in api
-  datasets: {
-    // dataset keys are the datasetID
-    // to allow O(1) access to update
-    // individual datasets in reducer
-    // functions.
-    [key: string]: Dataset
-  }
-  // adding this as a note
-  // organisms should be implemented
-  // as a special case of a dataset
-  // organisms: {
-  //   [key: string]: Dataset
-  // }
+// this is the container for information
+// that globally applies to an entire version,
+// the index in the array is the corresponding
+// version number, and the length of the array
+// is the number of versions which should be
+// in the register.
+export interface Version {
+  // the creation date of the version
+  date: string
+  // name of version; adding this as an
+  // example of future metadata we might need
+  name: string
 }
 
-// portfolio coming soon
-// export interface Portfolio {
-//   status: PortfolioStatus // don't save in api
-//   projects: {
-//     [key: string]: Project
-//   }
-// }
+// each Record in the register
+// is a collection of datapoints
+// which the user will see as a
+// row, or as an input form
+export interface Register {
+  [key: string]: Record
+}
+
+// a single record of data, a collection
+// of datapoints which the user sees
+// as a row in a table
+export interface Record {
+  [key: string]: Datapoint
+}
+
+// a single component of a record; the user
+// sees this as a cell in the dataset table
+// or in a form-like interface
+export interface Datapoint {
+  // human readable label
+  displayValue: string | number
+  // value of the datapoint inside our system,
+  // where that needs to be different; for
+  // example to implement links to other data
+  // such as organisms or locations
+  dataValue: string
+  // validation report response
+  report?: {
+    pass: ReportScore
+    message: string
+  }
+  // if this datapoint was modified
+  // in the active version
+  modified?: boolean
+  // userID
+  modifiedBy?: string
+  // version number where this
+  // datapoint was last modified.
+  // it is valid for any version
+  // higher than this number
+  version: number
+  // to access versions before
+  // the version number, traverse
+  // the linked list.
+  previous?: Datapoint
+}
+
+// the status states of the overall
+// datasets object
+export enum PortfolioStatus {
+  Initial,
+  Loading,
+  Loaded,
+  NetworkError,
+}
+
+// the status states of the overall
+// datasets object
+export enum ProjectStatus {
+  Initial,
+  Loading,
+  Loaded,
+  NetworkError,
+}
+
+// all possible statuses for a
+// dataset in the frontend
+export enum DatasetStatus {
+  Unsaved,
+  Saving,
+  Saved,
+  Error,
+}
+
+// all possible statuses for a register
+// relative to the server
+export enum RegisterStatus {
+  Unsaved,
+  Saving,
+  Saved,
+  Error,
+  Loading,
+}
+
+export enum ReportScore {
+  fail,
+  pass,
+  warning,
+}
