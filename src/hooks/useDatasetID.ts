74fea6c2c506f902efcac7383317bdfa4462f0d1
Ryan <rzimmerman@talusanalytics.com>
2022-07-15 10:01:36 -0300

    Hook to pull datasetID and throw error
    This hook returns type string instead of string | undefined

---
+++ src/hooks/useDatasetID.ts
@@ -0,0 +1,12 @@
+import { useParams } from 'react-router-dom'
+
+// take dataset ID from url params
+// and throw error if it is undefined
+const useDatasetID = () => {
+  const { id: datasetID } = useParams()
+  if (!datasetID) throw new Error('Dataset ID not found in url params')
+
+  return datasetID
+}
+
+export default useDatasetID
