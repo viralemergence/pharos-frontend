6f479c793ff4d736fa773721fc4d89cba757b521
Ryan <rzimmerman@talusanalytics.com>
2022-07-15 10:43:16 -0300

    Now that useParams is removed everywhere, fixing id in the url

--- src/hooks/dataset/useDatasetID.ts
+++ src/hooks/dataset/useDatasetID.ts
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 // take dataset ID from url params
 // and throw error if it is undefined
 const useDatasetID = () => {
-  const { id: datasetID } = useParams()
+  const { datasetID } = useParams()
   if (!datasetID) throw new Error('Dataset ID not found in url params')
 
   return datasetID
