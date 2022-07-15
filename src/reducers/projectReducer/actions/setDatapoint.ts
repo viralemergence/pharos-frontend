a564a52a844692c1539775a165713f0fd08090fd
David Rosado <drosado@talusanalytics.com>
2022-07-15 10:30:34 -0400

    Adding datapoint

---
+++ src/reducers/projectReducer/actions/setDatapoint.ts
@@ -0,0 +1,19 @@
+import { ActionFunction, ProjectActions } from '../projectReducer'
+import { Datapoint } from '../types'
+
+export interface SetDatapointPayload {
+  datasetID: string
+  datapoint: Datapoint
+}
+
+export interface SetDatapointAction {
+  type: ProjectActions.SetDatapoint
+  payload: SetDatapointPayload
+}
+
+const setDatapoint: ActionFunction<SetDatapointPayload> = (state, payload) => {
+  console.log(payload)
+  return state
+}
+
+export default setDatapoint
