a0598cb98b0a210ea620e240a7feba985c3a2066
Ryan <rzimmerman@talusanalytics.com>
2022-07-15 11:59:01 -0300

    Updating cell formatter to remove modified flag

--- src/components/AuthenticatedViews/DatasetEditor/DatasetGrid/formatters/SimpleFormatter.tsx
+++ src/components/AuthenticatedViews/DatasetEditor/DatasetGrid/formatters/SimpleFormatter.tsx
@@ -1,15 +1,22 @@
+import useDataset from 'hooks/dataset/useDataset'
 import React from 'react'
 import { FormatterProps } from 'react-data-grid'
 import { Record } from 'reducers/projectReducer/types'
 
-const SimpleFormatter = ({ column, row }: FormatterProps<Record, Record>) => (
-  <span
-    style={{
-      backgroundColor: row[column.key].modified ? 'orange' : 'white',
-    }}
-  >
-    {row[column.key].displayValue}
-  </span>
-)
+const SimpleFormatter = ({ column, row }: FormatterProps<Record, Record>) => {
+  const { versions } = useDataset()
+  const datapoint = row[column.key]
+
+  return (
+    <span
+      style={{
+        backgroundColor:
+          datapoint.version > versions.length ? 'orange' : 'white',
+      }}
+    >
+      {datapoint.displayValue}
+    </span>
+  )
+}
 
 export default SimpleFormatter
