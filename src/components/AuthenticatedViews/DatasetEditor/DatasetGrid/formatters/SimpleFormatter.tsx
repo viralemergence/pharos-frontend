897368083878f08ee95fee4361c27b2781363195
Ryan <rzimmerman@talusanalytics.com>
2022-07-15 12:16:11 -0300

    Cleaning up and simplifying the SimpleFormatter

--- src/components/AuthenticatedViews/DatasetEditor/DatasetGrid/formatters/SimpleFormatter.tsx
+++ src/components/AuthenticatedViews/DatasetEditor/DatasetGrid/formatters/SimpleFormatter.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 import { FormatterProps } from 'react-data-grid'
 import { Record } from 'reducers/projectReducer/types'
 
-const SimpleFormatter = ({ column, row }: FormatterProps<Record, Record>) => {
+const SimpleFormatter = ({
+  column,
+  row: { [column.key]: datapoint },
+}: FormatterProps<Record>) => {
   const { versions } = useDataset()
-  const datapoint = row[column.key]
 
   return (
     <span
