5a3b5fb8f4ca2d6be8224e256380080d78d76509
Ryan <rzimmerman@talusanalytics.com>
2022-07-15 18:05:13 -0300

    Fixing the create daatapoint case

--- src/reducers/projectReducer/actions/setDatapoint.ts
+++ src/reducers/projectReducer/actions/setDatapoint.ts
@@ -1,5 +1,5 @@
 import { ActionFunction, ProjectActions } from '../projectReducer'
-import { Datapoint } from '../types'
+import { Datapoint, Project } from '../types'
 
 export interface SetDatapointPayload {
   datasetID: string
@@ -13,29 +13,42 @@ export interface SetDatapointAction {
   payload: SetDatapointPayload
 }
 
-const setDatapoint: ActionFunction<SetDatapointPayload> = (state, payload) => {
+const setDatapoint: ActionFunction<SetDatapointPayload> = (
+  state,
+  payload
+  // I'm specifying a return type here to get
+  // errors if the return types aren't Projects
+): Project => {
   console.log(payload.recordKey)
   console.log(payload.datapointKey)
 
   // Datapoint does not exist -> create datapoint
-  if (payload.datapoint.version === undefined) {
+  if (payload.datapoint.version === undefined)
     return {
       ...state,
       datasets: {
         ...state.datasets,
         [payload.datasetID]: {
           ...state.datasets[payload.datasetID],
-          [payload.recordKey]: {
-            ...state.datasets[payload.datasetID].register[payload.recordKey],
-            [payload.datapointKey]: {
-              dataValue: payload.datapoint,
-              version: state.datasets[payload.datasetID].activeVersion,
+          // need to go in to the register
+          register: {
+            ...state.datasets[payload.datasetID].register,
+            [payload.recordKey]: {
+              ...state.datasets[payload.datasetID].register[payload.recordKey],
+              [payload.datapointKey]: {
+                // we have to give all required keys
+                // default values so it's impossible
+                // to create datapoints without them
+                displayValue: '',
+                dataValue: '',
+                version: state.datasets[payload.datasetID].activeVersion,
+                ...payload.datapoint,
+              },
             },
           },
         },
       },
     }
-  }
 
   if (
     // Datapoint exists and has not been edited
