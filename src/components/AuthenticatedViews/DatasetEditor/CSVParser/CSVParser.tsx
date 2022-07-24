609eebbbbfd58fb8e7a2fc7c62edc4d150c34e68
Ryan <rzimmerman@talusanalytics.com>
2022-07-24 00:00:25 -0300

    Cleaning up CSVParser component imports and stuff

--- src/components/AuthenticatedViews/DatasetEditor/CSVParser/CSVParser.tsx
+++ src/components/AuthenticatedViews/DatasetEditor/CSVParser/CSVParser.tsx
@@ -1,19 +1,12 @@
 import React from 'react'
-
-import Papa from 'papaparse'
 import { FileUploader } from 'react-drag-drop-files'
 
-import { RegisterStatus } from 'reducers/projectReducer/types'
-
-import { ProjectActions } from 'reducers/projectReducer/projectReducer'
-
 import useUser from 'hooks/useUser'
-import useProjectDispatch from 'hooks/project/useProjectDispatch'
 import useDatasetID from 'hooks/dataset/useDatasetID'
-import parseFile from './parse'
+import useProjectDispatch from 'hooks/project/useProjectDispatch'
 import useProject from 'hooks/project/useProject'
 
-// import saveVersion from 'api/uploadVersion'
+import parseFile from './parse'
 
 const fileTypes = ['CSV']
 
