37bf170feef8e1d041b49e5d4ea0b2aaf8a8789b
Ryan <rzimmerman@talusanalytics.com>
2022-07-27 15:13:19 -0300

    Cleaning up old code

--- src/reducers/projectReducer/actions/setRegisterStatus.ts
+++ src/reducers/projectReducer/actions/setRegisterStatus.ts
@@ -14,38 +14,15 @@ export interface SetRegisterStatusAction {
 const setRegisterStatus: ActionFunction<SetRegisterStatusPayload> = (
   state,
   payload
-) => {
-  // const activeVersion = state.datasets[payload.datasetID].activeVersion
-  // const nextVersions = [...(state.datasets[payload.datasetID].versions ?? [])]
-
-  // if (payload.status === VersionStatus.Saved) {
-  //   console.log('changing to saved status, resetting modified')
-  //   nextVersions[activeVersion].rows = nextVersions[activeVersion].rows?.map(
-  //     row => {
-  //       for (const key in row) {
-  //         row[key] = { ...row[key], unsaved: false }
-  //       }
-  //       return row
-  //     }
-  //   )
-  // }
-
-  // nextVersions[activeVersion] = {
-  //   ...nextVersions[activeVersion],
-  //   status: payload.status,
-  // }
-
-  return {
-    ...state,
-    datasets: {
-      ...state.datasets,
-      [payload.datasetID]: {
-        ...state.datasets[payload.datasetID],
-        // have to unpack the "status" value from payload
-        registerStatus: payload.status,
-      },
+) => ({
+  ...state,
+  datasets: {
+    ...state.datasets,
+    [payload.datasetID]: {
+      ...state.datasets[payload.datasetID],
+      registerStatus: payload.status,
     },
-  }
-}
+  },
+})
 
 export default setRegisterStatus
