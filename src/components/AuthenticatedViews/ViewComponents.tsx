e446b503a0860b48b436fd9c6da7bf023d87e609
Ryan <rzimmerman@talusanalytics.com>
2022-07-27 19:31:15 -0300

    Fixing height of datasets scroll

--- src/components/AuthenticatedViews/ViewComponents.tsx
+++ src/components/AuthenticatedViews/ViewComponents.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const Content = styled.div`
   overflow-y: scroll;
   padding: 40px;
+  height: calc(100vh - 100px);
 `
 export const TopBar = styled.div`
   display: flex;
