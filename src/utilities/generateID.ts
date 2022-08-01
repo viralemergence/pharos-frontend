74e1badae5a3ad568f879b42cd1779fa6684a9ca
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-08-01 20:03:12 -0300

    Adding functions to generate all the random IDs
    working on #28

---
+++ src/utilities/generateID.ts
@@ -0,0 +1,16 @@
+import { customAlphabet } from 'nanoid'
+
+const nanoid = customAlphabet(
+  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+  10
+)
+
+const projectID = () => 'prj' + nanoid()
+
+const datasetID = () => 'set' + nanoid()
+
+const recordID = () => 'rec' + nanoid()
+
+const datapointID = () => 'dat' + nanoid()
+
+export { projectID, datasetID, recordID, datapointID }
