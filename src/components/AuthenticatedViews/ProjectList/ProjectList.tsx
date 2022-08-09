b28a961440b65c69d22d80f31d6b91418a1eca5e
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-08-09 11:34:39 -0300

    Getting a few components of the project list page down

--- src/components/AuthenticatedViews/ProjectList/ProjectList.tsx
+++ src/components/AuthenticatedViews/ProjectList/ProjectList.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+import MainGrid from 'components/layout/MainGrid'
+import Sidebar from 'components/Sidebar/Sidebar'
+import MintButton from 'components/ui/MintButton'
+import Modal from 'components/ui/Modal'
+
+import { Content, TopBar } from '../ViewComponents'
+import CreateProjectForm from './CreateProjectForm/CreateProjectForm'
+import ProjectsTable from './ProjectsTable/ProjectsTable'
+
+const H1 = styled.h1`
+  ${({ theme }) => theme.h3}
+  margin: 0;
+`
 
 const ProjectList = () => {
-  return <p>List Projects page</p>
+  const [createModalOpen, setCreateModalOpen] = useState(false)
+
+  return (
+    <MainGrid>
+      <Sidebar />
+      <Content>
+        <TopBar>
+          <H1>My Datasets</H1>
+          <MintButton onClick={() => setCreateModalOpen(true)}>
+            + New Project
+          </MintButton>
+          <Modal closeable open={createModalOpen} setOpen={setCreateModalOpen}>
+            <CreateProjectForm />
+          </Modal>
+        </TopBar>
+        <ProjectsTable />
+      </Content>
+    </MainGrid>
+  )
 }
 
 export default ProjectList
