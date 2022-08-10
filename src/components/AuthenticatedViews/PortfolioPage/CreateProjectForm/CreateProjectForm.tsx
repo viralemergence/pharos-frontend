07f8f2a5bb3dbf907c35e3149404d95d63d27204
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-08-10 10:02:30 -0300

    Fixing text style and only allowing vertical resizing

--- src/components/AuthenticatedViews/PortfolioPage/CreateProjectForm/CreateProjectForm.tsx
+++ src/components/AuthenticatedViews/PortfolioPage/CreateProjectForm/CreateProjectForm.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import useUser from 'hooks/useUser'
 import MintButton from 'components/ui/MintButton'
+import Label from 'components/ui/InputLabel'
+import Input from 'components/ui/Input'
 
 const Form = styled.form`
   width: 500px;
@@ -19,13 +21,18 @@ const CreateProjectForm = () => {
   const user = useUser()
   const [formMessage, setFormMessage] = useState('')
 
-  const handleSubmit = () => {
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
     alert('create project')
   }
 
   return (
     <Form onSubmit={handleSubmit}>
       <H1>Create Project</H1>
+      <Label>
+        Project name
+        <Input type="text" name="name" autoFocus />
+      </Label>
       <p style={{ margin: 0, padding: 0 }}>{formMessage}</p>
       <MintButton type="submit" style={{ marginLeft: 'auto' }}>
         Create
--- src/components/ui/Textarea.tsx
+++ src/components/ui/Textarea.tsx
@@ -4,11 +4,13 @@ const Textarea = styled.textarea`
   border: none;
   border: 2px solid ${({ theme }) => theme.darkPurple};
   width: 100%;
+  max-width: 100%;
+  min-width: 100%;
   padding: 10px 20px;
   margin-top: 5px;
   background: ${({ theme }) => theme.veryLightGray};
   color: ${({ theme }) => theme.darkPurpleWhiter};
-  font-size: 20px;
+  ${({ theme }) => theme.smallParagraph};
 `
 
 export default Textarea
