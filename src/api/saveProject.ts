99d5a8aeb8565d36803fee33624e8bcc834243ab
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-08-11 15:40:03 -0300

    Correcting saveProject URL

--- src/api/saveProject.ts
+++ src/api/saveProject.ts
@@ -1,7 +1,7 @@
 import { Project } from 'reducers/projectReducer/types'
 
 const saveProject = async (payload: Project) => {
-  const response = await fetch(`${process.env.GATSBY_API_URL}/save-dataset`, {
+  const response = await fetch(`${process.env.GATSBY_API_URL}/save-project`, {
     method: 'POST',
     body: JSON.stringify(payload),
   }).catch(error => console.log(error))
