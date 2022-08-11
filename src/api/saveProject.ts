90884fdfd79eb90ca22a47d1a426e2d45ec71ec1
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-08-11 13:13:27 -0300

    Adding quick saveDataset api function

---
+++ src/api/saveProject.ts
@@ -0,0 +1,13 @@
+import { Project } from 'reducers/projectReducer/types'
+
+const saveProject = async (payload: Project) => {
+  const response = await fetch(`${process.env.GATSBY_API_URL}/save-dataset`, {
+    method: 'POST',
+    body: JSON.stringify(payload),
+  }).catch(error => console.log(error))
+
+  if (!response || !response.ok) return false
+  return true
+}
+
+export default saveProject
