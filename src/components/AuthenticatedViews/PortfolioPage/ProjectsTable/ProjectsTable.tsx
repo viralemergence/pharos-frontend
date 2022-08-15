110468a216818d591e4d1f0088c79bab0d181805
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-08-15 14:20:06 -0300

    Quick projects Table columsn

--- src/components/AuthenticatedViews/PortfolioPage/ProjectsTable/ProjectsTable.tsx
+++ src/components/AuthenticatedViews/PortfolioPage/ProjectsTable/ProjectsTable.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
-import ListTable, { HeaderRow } from 'components/ListTable/ListTable'
+import ListTable, { HeaderRow, RowLink } from 'components/ListTable/ListTable'
+import useProjecsObj from 'hooks/project/useProjectsObj'
+import useProjectDispatch from 'hooks/project/useProjectDispatch'
+import { ProjectActions } from 'reducers/projectReducer/projectReducer'
+import { Project } from 'reducers/projectReducer/types'
 
 const ProjectsTable = () => {
+  const projectsObj = useProjecsObj()
+  const projectDispatch = useProjectDispatch()
+
+  console.log({ projectsObj })
+  console.log(JSON.stringify(projectsObj))
+
+  const handleClick = (project: Project) => {
+    projectDispatch({
+      type: ProjectActions.SetProject,
+      payload: project,
+    })
+  }
+
   return (
     <ListTable columnTemplate="repeat(7, 1fr)">
       <HeaderRow>
@@ -14,6 +31,18 @@ const ProjectsTable = () => {
         <div># of organisms</div>
         <div>Last updated</div>
       </HeaderRow>
+      {Object.values(projectsObj).map(project => (
+        <RowLink
+          key={project.projectID}
+          onClick={() => handleClick(project)}
+          to={`/project/${project.projectID}`}
+        >
+          <div>{project.projectName}</div>
+          <div>start date</div>
+          <div>{project.projectType}</div>
+          <div>{project.surveillanceType}</div>
+        </RowLink>
+      ))}
     </ListTable>
   )
 }
