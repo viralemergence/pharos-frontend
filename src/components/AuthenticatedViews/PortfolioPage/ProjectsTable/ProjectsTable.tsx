1094befcfd71066b980747661632df1956eb5838
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-08-26 16:01:04 -0300

    Adding new columns on portfolio page

--- src/components/AuthenticatedViews/PortfolioPage/ProjectsTable/ProjectsTable.tsx
+++ src/components/AuthenticatedViews/PortfolioPage/ProjectsTable/ProjectsTable.tsx
@@ -24,11 +24,10 @@ const ProjectsTable = () => {
     <ListTable columnTemplate="repeat(7, 1fr)">
       <HeaderRow>
         <div>Project name</div>
-        <div>Start date</div>
-        <div>Type</div>
+        <div>Project type</div>
         <div>Surveillance type</div>
+        <div>Surveillance status</div>
         <div># of datasets</div>
-        <div># of organisms</div>
         <div>Last updated</div>
       </HeaderRow>
       {Object.values(projectsObj).map(project => (
@@ -38,9 +37,11 @@ const ProjectsTable = () => {
           to={`/projects/${project.projectID}`}
         >
           <div>{project.projectName}</div>
-          <div>start date</div>
-          <div>{project.projectType}</div>
-          <div>{project.surveillanceType}</div>
+          <div>{project.projectType || '—'}</div>
+          <div>{project.surveillanceType || '—'}</div>
+          <div>{project.surveillanceStatus || '—'}</div>
+          <div>{project.datasetIDs.length}</div>
+          <div>{project.lastUpdated || '—'}</div>
         </RowLink>
       ))}
     </ListTable>
