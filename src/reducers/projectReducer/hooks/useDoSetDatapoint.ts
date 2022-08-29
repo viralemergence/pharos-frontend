631d9bee60d9430fabdca19e7c60d22ab5df8e46
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-08-29 00:15:29 -0300

    implementing doSetDatapoint using the new action creator pattern

---
+++ src/reducers/projectReducer/hooks/useDoSetDatapoint.ts
@@ -0,0 +1,48 @@
+import useDatasetID from 'hooks/dataset/useDatasetID'
+import useProjectDispatch from 'hooks/project/useProjectDispatch'
+import useUser from 'hooks/useUser'
+import { ProjectActions } from '../projectReducer'
+import { Datapoint } from '../types'
+
+interface DoSetDatapointProps {
+  recordID: string
+  datapointID: string
+  displayValue: string
+  dataValue: Datapoint['dataValue']
+}
+
+const useDoSetDatapoint = () => {
+  const projectDispatch = useProjectDispatch()
+  const datasetID = useDatasetID()
+  const user = useUser()
+
+  if (!user.data?.researcherID)
+    throw new Error('Cannot set datapoint when user data is undefined ')
+
+  const modifiedBy = user.data.researcherID
+
+  const doSetDatapoint = ({
+    recordID,
+    datapointID,
+    displayValue,
+    dataValue,
+  }: DoSetDatapointProps) => {
+    projectDispatch({
+      type: ProjectActions.SetDatapoint,
+      payload: {
+        datasetID,
+        recordID,
+        datapointID,
+        datapoint: {
+          displayValue,
+          dataValue,
+          modifiedBy,
+        },
+      },
+    })
+  }
+
+  return doSetDatapoint
+}
+
+export default useDoSetDatapoint
