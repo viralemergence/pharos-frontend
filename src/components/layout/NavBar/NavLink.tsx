d18441d6581b42f836adf1cdf37aca88210c4759
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-09-06 09:00:49 -0300

    Revert "Merge branch 'dev' into feature/list-projects"
    This reverts commit de2090dda82042dabc1a01a427731bae161abc52, reversing
    changes made to f18f1e63c6403d9e279fad89a31620c35e35f64e.

--- src/components/layout/NavBar/NavLink.tsx
+++ src/components/layout/NavBar/NavLink.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { NavLink as ReactRouterLink } from 'react-router-dom'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 const Li = styled.li`
   display: flex;
 `
 
-const linkStyle = css`
+const StyledLink = styled(Link)`
   color: white !important;
   padding: 14px;
   text-decoration: none;
@@ -20,16 +19,6 @@ const linkStyle = css`
     color: ${({ theme }) => theme.lightPurple} !important;
   }
 `
-
-// applying the same styling to reach-router links
-// and react-router-dom links so they look the same
-const StyledLink = styled(Link)`
-  ${linkStyle}
-`
-const ReactRouterStyledLink = styled(ReactRouterLink)`
-  ${linkStyle}
-`
-
 const defaultActiveStyle = {
   fontWeight: '600',
 }
@@ -39,40 +28,18 @@ interface Props {
   activeStyle?: object
   children: React.ReactNode
   className?: string
-  reactRouterLink?: boolean
 }
 
-const NavLink = ({
-  to,
-  activeStyle,
-  className,
-  reactRouterLink,
-  ...props
-}: Props) => (
+const NavLink = ({ to, activeStyle, className, ...props }: Props) => (
   <Li>
-    {reactRouterLink ? (
-      <>
-        <ReactRouterStyledLink
-          {...{ props }}
-          to={to}
-          style={({ isActive }) =>
-            isActive ? activeStyle || defaultActiveStyle : {}
-          }
-          className={className}
-        >
-          {props.children}
-        </ReactRouterStyledLink>
-      </>
-    ) : (
-      <StyledLink
-        {...{ props }}
-        to={to}
-        activeStyle={activeStyle || defaultActiveStyle}
-        className={className}
-      >
-        {props.children}
-      </StyledLink>
-    )}
+    <StyledLink
+      {...{ props }}
+      to={to}
+      activeStyle={activeStyle || defaultActiveStyle}
+      className={className}
+    >
+      {props.children}
+    </StyledLink>
   </Li>
 )
 
