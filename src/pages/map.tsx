0dab178300d9c2e9f933da4b8fe00e8c74ca1b82
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-09-09 18:00:56 -0300

    Adding map to map page component #60

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -4,13 +4,14 @@ import CMS from '@talus-analytics/library.airtable-cms'
 import Providers from 'components/layout/Providers'
 
 import NavBar from 'components/layout/NavBar/NavBar'
+import MapPage from 'components/MapPage/MapPage'
 
-const MapPage = (): JSX.Element => (
+const Map = (): JSX.Element => (
   <Providers>
     <CMS.SEO />
     <NavBar />
-    <h1>Map</h1>
+    <MapPage />
   </Providers>
 )
 
-export default MapPage
+export default Map
