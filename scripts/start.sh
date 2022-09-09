17dad1a1cbb8b3f2870d6d38cef85e6b8b550c5b
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-09-09 18:00:06 -0300

    Making start script request pharos-specific mapbox api key from secretsmanager
    Issue #60

--- scripts/start.sh
+++ scripts/start.sh
@@ -20,6 +20,10 @@ export AIRTABLE_API_KEY=$(
   aws secretsmanager get-secret-value --secret-id airtable-api-key --region us-west-1 |\
   jq  -r .SecretString | jq -r .AIRTABLE_API_KEY\
 )
+export GATSBY_MAPBOX_API_KEY=$(
+  aws secretsmanager get-secret-value --secret-id pharos-mapbox-api-key --region us-west-1 |\
+  jq  -r .SecretString | jq -r .MAPBOX_API_KEY\
+)
 
 if [ "$reset" == "1" ]; then 
   bvm upgrade;
