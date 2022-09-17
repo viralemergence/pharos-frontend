2e22a720eb08e1a5026678e3331447e764962304
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-09-16 21:27:35 -0300

    Moving styles into a separate file so the base documentation style is reusable

--- src/templates/documentation/default.tsx
+++ src/templates/documentation/default.tsx
@@ -5,58 +5,10 @@ import { graphql } from 'gatsby'
 import Providers from 'components/layout/Providers'
 import NavBar from 'components/layout/NavBar/NavBar'
 import Main from 'components/layout/Main'
+import { baseStyle } from './styles'
 
 const Content = styled.div`
-  max-width: 1000px;
-  width: 100%;
-  margin: auto;
-
-  h1 {
-    ${({ theme }) => theme.h1}
-  }
-
-  h2 {
-    ${({ theme }) => theme.h2}
-  }
-
-  h3 {
-    ${({ theme }) => theme.h3}
-  }
-
-  p {
-    ${({ theme }) => theme.bigParagraph}
-  }
-
-  table {
-    width: 100%;
-    border-collapse: collapse;
-
-    th {
-      text-align: left;
-      background-color: rgba(170, 233, 220, 0.33);
-      border-bottom: thin solid rgba(170, 233, 220, 1);
-
-      &:first-of-type {
-        border-top-left-radius: 10px;
-      }
-      &:last-of-type {
-        border-top-right-radius: 10px;
-      }
-    }
-
-    th,
-    td {
-      padding: 12px 15px;
-    }
-
-    tr {
-      border-bottom: thin solid #dddddd;
-    }
-
-    tr:nth-child(odd) > td {
-      background-color: rgba(0, 0, 0, 0.05);
-    }
-  }
+  ${baseStyle}
 `
 
 const DocsDefault = ({ data }: { data: Data }) => {
