449a4a3f361e3fbae86ee7acfb5cf506448d8942
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-09-27 15:12:29 -0400

    Generalizing small text button for reuse #50

--- src/components/ui/TextButton.tsx
+++ src/components/ui/TextButton.tsx
@@ -1,11 +1,18 @@
 import styled from 'styled-components'
 
-const TextButton = styled.button<{ primary?: boolean }>`
-  ${({ theme }) => theme.smallParagraph};
+const TextButton = styled.button<{ primary?: boolean; small?: boolean }>`
+  ${({ theme, small }) =>
+    small ? theme.extraSmallParagraph : theme.smallParagraph};
+
   margin: 0;
   background: none;
   border: none;
-  color: ${({ theme, primary }) => (primary ? theme.link : theme.black)};
+  color: ${({ theme, primary }) => (primary ? theme.link : theme.veryDarkGray)};
+
+  transition: 150ms ease;
+  &:hover {
+    color: ${({ theme, primary }) => (primary ? theme.black : theme.link)};
+  }
 `
 
 export default TextButton
