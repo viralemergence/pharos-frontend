8bd33a93690edba8667b75bdca21b43a6d8397b5
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-10-06 22:05:16 -0300

    Fixing useProject to draw on the useProjects hook and populate with initial value when state is empty

--- src/hooks/project/useProject.ts
+++ src/hooks/project/useProject.ts
@@ -1,11 +1,14 @@
-import useAppState from 'hooks/useAppState'
+import { projectInitialValue } from 'reducers/projectReducer/projectReducer'
 import useProjectID from './useProjectID'
+import useProjects from './useProjects'
 
 const useProject = () => {
-  const state = useAppState()
+  const projects = useProjects()
   const projectID = useProjectID()
 
-  return state.projects[projectID]
+  const project = projects[projectID] ?? projectInitialValue
+
+  return project
 }
 
 export default useProject
