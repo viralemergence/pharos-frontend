9de986d29c983c0ddb8383f42c8a07bd30add40d
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-10-06 22:26:37 -0300

    Convincing typescript that I'm ok with an explosion here in this case (I want it to break the build)

--- src/hooks/useDispatch.ts
+++ src/hooks/useDispatch.ts
@@ -1,11 +1,13 @@
 import { useContext } from 'react'
 import { ProjectContext } from 'reducers/projectReducer/projectContext'
+import { ProjectAction } from 'reducers/projectReducer/projectReducer'
 
-const useDispatch = () => {
+const useDispatch = (): React.Dispatch<ProjectAction> => {
   const context = useContext(ProjectContext)
 
   // dispatch should be null on the build server; calling
   // this on the build server can lead to rehydration bugs.
+  // @ts-expect-error Type 'null' is not assignable to type 'Dispatch<ProjectAction>'
   if (typeof window === 'undefined' && !context) return null
 
   if (!context?.dispatch) throw new Error('Project context not found')
