92905d63864d84d0a20613a32bd45a51c514043e
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-10-10 14:51:32 -0300

    Making both autosave functions no-op just to be save

--- src/hooks/register/useAutosaveDataset.ts
+++ src/hooks/register/useAutosaveDataset.ts
@@ -1,63 +1,65 @@
-import saveDataset from 'api/saveDataset'
-import useDataset from 'hooks/dataset/useDataset'
-import useDatasetID from 'hooks/dataset/useDatasetID'
-import useDispatch from 'hooks/useDispatch'
-import useUser from 'hooks/useUser'
-import { useEffect } from 'react'
-import { ProjectActions } from 'reducers/projectReducer/projectReducer'
-import { DatasetStatus } from 'reducers/projectReducer/types'
+// import saveDataset from 'api/saveDataset'
+// import useDataset from 'hooks/dataset/useDataset'
+// import useDatasetID from 'hooks/dataset/useDatasetID'
+// import useDispatch from 'hooks/useDispatch'
+// import useUser from 'hooks/useUser'
+// import { useEffect } from 'react'
+// import { ProjectActions } from 'reducers/projectReducer/projectReducer'
+// import { DatasetStatus } from 'reducers/projectReducer/types'
 
 const useAutosaveDataset = () => {
-  const projectDispatch = useDispatch()
-  const datasetID = useDatasetID()
-  const dataset = useDataset()
-  const user = useUser()
+  return () => null
 
-  useEffect(() => {
-    const save = async () => {
-      if (dataset.status !== DatasetStatus.Unsaved || !user.data?.researcherID)
-        return null
+  // const projectDispatch = useDispatch()
+  // const datasetID = useDatasetID()
+  // const dataset = useDataset()
+  // const user = useUser()
 
-      projectDispatch({
-        type: ProjectActions.SetDatasetStatus,
-        payload: {
-          datasetID,
-          status: DatasetStatus.Saving,
-        },
-      })
+  // useEffect(() => {
+  //   const save = async () => {
+  //     if (dataset.status !== DatasetStatus.Unsaved || !user.data?.researcherID)
+  //       return null
 
-      console.log('API Sync: Save Dataset')
-      // omit register, save everything else to the server
-      const datasetSaveData = { ...dataset }
-      delete datasetSaveData.registerStatus
-      delete datasetSaveData.register
-      delete datasetSaveData.status
+  //     projectDispatch({
+  //       type: ProjectActions.SetDatasetStatus,
+  //       payload: {
+  //         datasetID,
+  //         status: DatasetStatus.Saving,
+  //       },
+  //     })
 
-      console.log(datasetSaveData)
+  //     console.log('API Sync: Save Dataset')
+  //     // omit register, save everything else to the server
+  //     const datasetSaveData = { ...dataset }
+  //     delete datasetSaveData.registerStatus
+  //     delete datasetSaveData.register
+  //     delete datasetSaveData.status
 
-      const saved = await saveDataset(datasetSaveData)
+  //     console.log(datasetSaveData)
 
-      if (saved) {
-        projectDispatch({
-          type: ProjectActions.SetDatasetStatus,
-          payload: {
-            datasetID,
-            status: DatasetStatus.Saved,
-          },
-        })
-      } else {
-        projectDispatch({
-          type: ProjectActions.SetDatasetStatus,
-          payload: {
-            datasetID,
-            status: DatasetStatus.Error,
-          },
-        })
-      }
-    }
+  //     const saved = await saveDataset(datasetSaveData)
 
-    save()
-  }, [datasetID, dataset, projectDispatch, user.data?.researcherID])
+  //     if (saved) {
+  //       projectDispatch({
+  //         type: ProjectActions.SetDatasetStatus,
+  //         payload: {
+  //           datasetID,
+  //           status: DatasetStatus.Saved,
+  //         },
+  //       })
+  //     } else {
+  //       projectDispatch({
+  //         type: ProjectActions.SetDatasetStatus,
+  //         payload: {
+  //           datasetID,
+  //           status: DatasetStatus.Error,
+  //         },
+  //       })
+  //     }
+  //   }
+
+  //   save()
+  // }, [datasetID, dataset, projectDispatch, user.data?.researcherID])
 }
 
 export default useAutosaveDataset
--- src/hooks/register/useAutosaveRegister.ts
+++ src/hooks/register/useAutosaveRegister.ts
@@ -1,70 +1,72 @@
-import saveRegister from 'api/saveRegister'
-import useDataset from 'hooks/dataset/useDataset'
-import useDatasetID from 'hooks/dataset/useDatasetID'
-import useDispatch from 'hooks/useDispatch'
-import useUser from 'hooks/useUser'
-import { useEffect } from 'react'
-import { ProjectActions } from 'reducers/projectReducer/projectReducer'
-import { RegisterStatus } from 'reducers/projectReducer/types'
+// import saveRegister from 'api/saveRegister'
+// import useDataset from 'hooks/dataset/useDataset'
+// import useDatasetID from 'hooks/dataset/useDatasetID'
+// import useDispatch from 'hooks/useDispatch'
+// import useUser from 'hooks/useUser'
+// import { useEffect } from 'react'
+// import { ProjectActions } from 'reducers/projectReducer/projectReducer'
+// import { RegisterStatus } from 'reducers/projectReducer/types'
 
 const useAutosaveRegister = () => {
-  const projectDispatch = useDispatch()
-  const datasetID = useDatasetID()
-  const dataset = useDataset()
-  const user = useUser()
+  return () => null
 
-  useEffect(() => {
-    const save = async () => {
-      if (
-        dataset.registerStatus !== RegisterStatus.Unsaved ||
-        !dataset?.register ||
-        !user.data?.researcherID
-      )
-        return null
+  // const projectDispatch = useDispatch()
+  // const datasetID = useDatasetID()
+  // const dataset = useDataset()
+  // const user = useUser()
 
-      projectDispatch({
-        type: ProjectActions.SetRegisterStatus,
-        payload: {
-          datasetID,
-          status: RegisterStatus.Saving,
-        },
-      })
+  // useEffect(() => {
+  //   const save = async () => {
+  //     if (
+  //       dataset.registerStatus !== RegisterStatus.Unsaved ||
+  //       !dataset?.register ||
+  //       !user.data?.researcherID
+  //     )
+  //       return null
 
-      console.log('API Sync: Save Register')
-      const saved = await saveRegister({
-        datasetID,
-        researcherID: user.data.researcherID,
-        register: dataset.register,
-      }).catch(e => console.log(e))
+  //     projectDispatch({
+  //       type: ProjectActions.SetRegisterStatus,
+  //       payload: {
+  //         datasetID,
+  //         status: RegisterStatus.Saving,
+  //       },
+  //     })
 
-      if (saved) {
-        projectDispatch({
-          type: ProjectActions.SetRegisterStatus,
-          payload: {
-            datasetID,
-            status: RegisterStatus.Saved,
-          },
-        })
-      } else {
-        projectDispatch({
-          type: ProjectActions.SetRegisterStatus,
-          payload: {
-            datasetID,
-            status: RegisterStatus.Error,
-          },
-        })
-      }
-    }
+  //     console.log('API Sync: Save Register')
+  //     const saved = await saveRegister({
+  //       datasetID,
+  //       researcherID: user.data.researcherID,
+  //       register: dataset.register,
+  //     }).catch(e => console.log(e))
 
-    save()
-  }, [
-    datasetID,
-    projectDispatch,
-    dataset.register,
-    dataset.versions,
-    dataset.registerStatus,
-    user.data?.researcherID,
-  ])
+  //     if (saved) {
+  //       projectDispatch({
+  //         type: ProjectActions.SetRegisterStatus,
+  //         payload: {
+  //           datasetID,
+  //           status: RegisterStatus.Saved,
+  //         },
+  //       })
+  //     } else {
+  //       projectDispatch({
+  //         type: ProjectActions.SetRegisterStatus,
+  //         payload: {
+  //           datasetID,
+  //           status: RegisterStatus.Error,
+  //         },
+  //       })
+  //     }
+  //   }
+
+  //   save()
+  // }, [
+  //   datasetID,
+  //   projectDispatch,
+  //   dataset.register,
+  //   dataset.versions,
+  //   dataset.registerStatus,
+  //   user.data?.researcherID,
+  // ])
 }
 
 export default useAutosaveRegister
