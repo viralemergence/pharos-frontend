223f41083ac72696b71ccddcdbe9d4bf6604ea1e
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-11-14 14:58:58 -0300

    Adding first pass of saveRegister StorageFunction

---
+++ src/storage/storageFunctions/saveRegister.ts
@@ -0,0 +1,48 @@
+import localforage from 'localforage'
+
+import { DatasetID, Register } from 'reducers/stateReducer/types'
+import { StateActions } from 'reducers/stateReducer/stateReducer'
+
+import {
+  APIRoutes,
+  StorageFunction,
+  StorageMessagePayload,
+  StorageMessageStatus,
+} from 'storage/synchronizeMessageQueue'
+
+export type SaveRegister = StorageMessagePayload<
+  APIRoutes.saveRegister,
+  {
+    register: Register
+    datasetID: DatasetID
+  }
+>
+
+const saveRegister: StorageFunction<SaveRegister> = async (
+  key,
+  message,
+  dispatch
+) => {
+  dispatch({
+    type: StateActions.SetStorageMessageStatus,
+    payload: { key, status: StorageMessageStatus.Pending },
+  })
+
+  if (message.target === 'local') {
+    const key = `${message.data.datasetID}-register`
+    await localforage.setItem(key, message.data.register).catch(() =>
+      dispatch({
+        type: StateActions.SetStorageMessageStatus,
+        payload: { key, status: StorageMessageStatus.LocalStorageError },
+      })
+    )
+    dispatch({ type: StateActions.RemoveStorageMessage, payload: key })
+  } else {
+    dispatch({
+      type: StateActions.SetStorageMessageStatus,
+      payload: { key, status: StorageMessageStatus.NetworkError },
+    })
+  }
+}
+
+export default saveRegister
--- src/storage/synchronizeMessageQueue.ts
+++ src/storage/synchronizeMessageQueue.ts
@@ -3,6 +3,7 @@ import { StateAction } from 'reducers/stateReducer/stateReducer'
 import saveUser, { SaveUser } from './storageFunctions/saveUser'
 import saveProject, { SaveProject } from 'storage/storageFunctions/saveProject'
 import saveDataset, { SaveDataset } from './storageFunctions/saveDataset'
+import saveRegister, { SaveRegister } from './storageFunctions/saveRegister'
 
 export enum StorageMessageStatus {
   // when the api message is created
@@ -23,6 +24,7 @@ export enum APIRoutes {
   saveUser = 'save-user',
   saveProject = 'save-project',
   saveDataset = 'save-dataset',
+  saveRegister = 'save-register',
 }
 
 export interface StorageMessagePayload<T, K> {
@@ -68,6 +70,10 @@ const synchronizeMessageQueue = async (
 
         case APIRoutes.saveDataset:
           saveDataset(key, message, dispatch)
+          continue
+
+        case APIRoutes.saveRegister:
+          saveRegister(key, message, dispatch)
           continue
       }
     }
