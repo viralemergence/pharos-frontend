686f2864e46908d7299021cf8bf3d75c04d0f937
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-11-14 23:07:50 -0300

    Modifying setRegister to handle all situations and save register into local

--- src/reducers/stateReducer/actions/setRegister.ts
+++ src/reducers/stateReducer/actions/setRegister.ts
@@ -1,17 +1,100 @@
+import {
+  APIRoutes,
+  StorageMessageStatus,
+} from 'storage/synchronizeMessageQueue'
 import { ActionFunction, StateActions } from '../stateReducer'
-import { NodeStatus, Register } from '../types'
+import { DatasetID, NodeStatus, Register } from '../types'
+
+interface SetRegisterActionPayload {
+  source: 'local' | 'remote'
+  data: Register
+  datasetID: DatasetID
+}
 
 export interface SetRegisterAction {
   type: StateActions.SetRegister
-  payload: Register
+  payload: SetRegisterActionPayload
 }
 
-const setRegister: ActionFunction<Register> = (state, payload) => {
+const setRegister: ActionFunction<SetRegisterActionPayload> = (
+  state,
+  payload
+) => {
+  const { data: register, source, datasetID } = payload
+
+  // if we're loading from the indexedDB we can just set it directly
+  if (source === 'local')
+    return {
+      ...state,
+      register: {
+        ...state.register,
+        data: register,
+      },
+    }
+
+  // if source is remote and current state is empty, just set it and save to local
+  if (source === 'remote' && Object.entries(state.register).length === 0)
+    return {
+      ...state,
+      register: {
+        ...state.register,
+        data: register,
+      },
+      messageStack: {
+        ...state.messageStack,
+        [`${APIRoutes.saveRegister}_${datasetID}_local`]: {
+          route: APIRoutes.saveRegister,
+          status: StorageMessageStatus.Initial,
+          data: { register, datasetID },
+          target: 'local',
+        },
+      },
+    }
+
+  // if source is remote and we have a register already loaded we need to merge them
+  const nextRegister: Register = {}
+
+  // iterate over the records in the register
+  for (const [recordID, remoteRecord] of Object.entries(register)) {
+    const localRecord = state.register.data[recordID]
+    nextRegister[recordID] = localRecord
+
+    // iterate over the datapoints in the record
+    for (const [datapointID, remoteDatapoint] of Object.entries(remoteRecord)) {
+      const localDatapoint = localRecord[datapointID]
+
+      // overwrite the local datapoint with the remote datapoint
+      nextRegister[recordID][datapointID] = remoteDatapoint
+
+      // if the version strings match, continue (keep local)
+      if (localDatapoint.version === remoteDatapoint.version) continue
+
+      // parse the version strings into numbers
+      const localDate = Number(localRecord.version || 0)
+      const remoteDate = Number(remoteRecord.version || 0)
+
+      // if the local one is newer keep that
+      if (localDate > remoteDate) {
+        nextRegister[recordID][datapointID] = localDatapoint
+        continue
+      }
+    }
+  }
+
   return {
     ...state,
     register: {
-      ...state.register,
-      data: payload,
+      status: NodeStatus.Loaded,
+      data: nextRegister,
+    },
+    messageStack: {
+      ...state.messageStack,
+      [`${APIRoutes.saveRegister}_${datasetID}_local`]: {
+        route: APIRoutes.saveRegister,
+        status: StorageMessageStatus.Initial,
+        data: { register, datasetID },
+        target: 'local',
+      },
     },
   }
 }
