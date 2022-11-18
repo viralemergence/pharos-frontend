dfd2545b36239a198de875adc0f2599b21b6ecf2
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-11-18 15:07:43 -0300

    Make sure setUser function preserves prior values (only ever adds projects)

--- src/reducers/stateReducer/actions/setUser.ts
+++ src/reducers/stateReducer/actions/setUser.ts
@@ -1,14 +1,39 @@
 import { User } from '../types'
 import { ActionFunction, StateActions } from '../stateReducer'
+import { userInitialValue } from '../initialValues'
+import {
+  APIRoutes,
+  StorageMessageStatus,
+} from 'storage/synchronizeMessageQueue'
 
 export interface SetUserAction {
   type: StateActions.SetUser
   payload: User
 }
 
-const setUser: ActionFunction<User> = (state, payload) => ({
-  ...state,
-  user: payload,
-})
+const setUser: ActionFunction<User> = (state, payload) => {
+  const nextUser = { ...state.user }
+  if (payload.data) {
+    nextUser.data = { ...state.user.data, ...payload.data }
+    nextUser.data.projectIDs = [
+      ...(state.user.data?.projectIDs ?? []),
+      ...(payload.data.projectIDs ?? []),
+    ]
+  }
+
+  return {
+    ...state,
+    user: nextUser,
+    messageStack: {
+      ...state.messageStack,
+      [`${APIRoutes.saveUser}_user_local`]: {
+        route: APIRoutes.saveUser,
+        target: 'local',
+        status: StorageMessageStatus.Initial,
+        data: nextUser,
+      },
+    },
+  }
+}
 
 export default setUser
