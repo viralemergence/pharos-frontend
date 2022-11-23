3a0e0e150bfaa344bb7603c5117298a3034f296d
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-11-23 10:53:54 -0300

    These sections of the release button are related to deprecated versioning #89

--- src/components/AuthenticatedViews/DatasetEditor/ReleaseButton/ReleaseButton.tsx
+++ src/components/AuthenticatedViews/DatasetEditor/ReleaseButton/ReleaseButton.tsx
@@ -1,79 +1,70 @@
 import React from 'react'
 
-import { StateActions } from 'reducers/stateReducer/stateReducer'
-import {
-  DatasetReleaseStatus,
-  DatasetStatus,
-} from 'reducers/stateReducer/types'
-
 import MintButton from 'components/ui/MintButton'
 
 import useUser from 'hooks/useUser'
-import useDatasetID from 'hooks/dataset/useDatasetID'
 import useDataset from 'hooks/dataset/useDataset'
-import useDispatch from 'hooks/useDispatch'
-import getTimestamp from 'utilities/getTimestamp'
 
 const ReleaseButton = () => {
   const user = useUser()
-  const datasetID = useDatasetID()
-  const projectDispatch = useDispatch()
+  // const datasetID = useDatasetID()
+  // const projectDispatch = useDispatch()
 
   const dataset = useDataset()
 
   // don't render the update button if there are no datasets loaded,
   // the project is loading, or there are no versions in the dataset
-  if (!dataset || dataset.status === DatasetStatus.Error) return <></>
+  if (!dataset) return <></>
 
-  let buttonMessage = 'Release dataset'
-  let buttonDisabled = false
-  switch (true) {
-    // if there are no versions, we can publish
-    case dataset.versions.length === 0:
-      buttonMessage = 'Release dataset'
-      buttonDisabled = false
-      break
-    // if we're looking at an old version, it's published
-    case dataset.activeVersion < dataset.versions.length - 1:
-      buttonMessage = 'Dataset released'
-      buttonDisabled = true
-      break
-    case dataset.highestVersion > dataset.activeVersion:
-      buttonMessage = 'Release dataset'
-      buttonDisabled = false
-      break
-    default:
-      buttonMessage = 'Dataset released'
-      buttonDisabled = true
-      break
-  }
+  const buttonMessage = 'Release dataset'
+  const buttonDisabled = false
+  // switch (true) {
+  //   // if there are no versions, we can publish
+  //   case dataset.versions.length === 0:
+  //     buttonMessage = 'Release dataset'
+  //     buttonDisabled = false
+  //     break
+  //   // if we're looking at an old version, it's published
+  //   case dataset.activeVersion < dataset.versions.length - 1:
+  //     buttonMessage = 'Dataset released'
+  //     buttonDisabled = true
+  //     break
+  //   case dataset.highestVersion > dataset.activeVersion:
+  //     buttonMessage = 'Release dataset'
+  //     buttonDisabled = false
+  //     break
+  //   default:
+  //     buttonMessage = 'Dataset released'
+  //     buttonDisabled = true
+  //     break
+  // }
 
   const onClickUpdate = async (e: React.SyntheticEvent<HTMLButtonElement>) => {
     e.preventDefault()
 
     if (!user.data?.researcherID) throw new Error('User data not found')
 
-    const lastUpdated = getTimestamp()
+    // const lastUpdated = getTimestamp()
 
-    projectDispatch({
-      type: StateActions.SetDatasetReleaseStatus,
-      payload: {
-        datasetID,
-        lastUpdated,
-        releaseStatus: DatasetReleaseStatus.Released,
-      },
-    })
+    // projectDispatch({
+    //   type: StateActions.SetDatasetReleaseStatus,
+    //   payload: {
+    //     datasetID,
+    //     lastUpdated,
+    //     releaseStatus: DatasetReleaseStatus.Released,
+    //   },
+    // })
 
-    projectDispatch({
-      type: StateActions.CreateVersion,
-      payload: {
-        datasetID,
-        version: {
-          date: String(new Date().toUTCString()),
-          name: String(new Date().toUTCString()),
-        },
-      },
-    })
+    // projectDispatch({
+    //   type: StateActions.CreateVersion,
+    //   payload: {
+    //     datasetID,
+    //     version: {
+    //       date: String(new Date().toUTCString()),
+    //       name: String(new Date().toUTCString()),
+    //     },
+    //   },
+    // })
   }
 
   return (
