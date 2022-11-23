4e4047a110e9563778b787019a61221e003afcbc
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-11-23 12:32:53 -0300

    Commenting out unused sections of updateUser #89

--- src/reducers/stateReducer/actions/updateUser.ts
+++ src/reducers/stateReducer/actions/updateUser.ts
@@ -1,10 +1,10 @@
 import { User } from '../types'
 import { ActionFunction, StateActions } from '../stateReducer'
 
-import {
-  APIRoutes,
-  StorageMessageStatus,
-} from 'storage/synchronizeMessageQueue'
+// import {
+//   APIRoutes,
+//   StorageMessageStatus,
+// } from 'storage/synchronizeMessageQueue'
 
 export interface UpdateUserAction {
   type: StateActions.UpdateUser
@@ -12,27 +12,27 @@ export interface UpdateUserAction {
 }
 
 const updateUser: ActionFunction<User> = (state, payload) => {
-  const nextUser = { ...state.user }
-  if (payload.data) {
-    nextUser.data = { ...state.user.data, ...payload.data }
-    nextUser.data.projectIDs = [
-      ...(state.user.data?.projectIDs ?? []),
-      ...(payload.data.projectIDs ?? []),
-    ]
-  }
+  // const nextUser = { ...state.user }
+  // if (payload.data) {
+  //   nextUser.data = { ...state.user.data, ...payload.data }
+  //   nextUser.data.projectIDs = [
+  //     ...(state.user.data?.projectIDs ?? []),
+  //     ...(payload.data.projectIDs ?? []),
+  //   ]
+  // }
 
   return {
     ...state,
-    user: nextUser,
-    messageStack: {
-      ...state.messageStack,
-      [`${APIRoutes.saveUser}_user_local`]: {
-        route: APIRoutes.saveUser,
-        target: 'local',
-        status: StorageMessageStatus.Initial,
-        data: nextUser,
-      },
-    },
+    user: payload,
+    // messageStack: {
+    //   ...state.messageStack,
+    //   [`${APIRoutes.saveUser}_user_local`]: {
+    //     route: APIRoutes.saveUser,
+    //     target: 'local',
+    //     status: StorageMessageStatus.Initial,
+    //     data: nextUser,
+    //   },
+    // },
   }
 }
 
