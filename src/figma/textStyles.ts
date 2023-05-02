397ff2b1b08c90fd143125c2f7dc390005360f1a
ryan-talus <rzimmerman@talusanalytics.com>
2023-05-01 21:56:22 -0300

    Updating text styles

--- src/figma/textStyles.ts
+++ src/figma/textStyles.ts
@@ -1,82 +1,107 @@
 export default {
   h1: `
-	font-size:36px;
-	font-family:"Poppins", helvetica, sans-serif;
-	font-weight:400;
-	font-style:normal;
-	text-decoration:none;
-	text-transform:none;
-	`,
+	font-size: 37px;
+	font-family: "Overpass";
+	font-weight: 400;
+	font-style: normal;
+	line-height: 47px;
+	text-decoration: none;
+	text-transform: none;
+`,
   h2: `
-	font-size:28px;
-	font-family:"Poppins", helvetica, sans-serif;
-	font-weight:400;
-	font-style:normal;
-	text-decoration:none;
-	text-transform:none;
-	`,
+	font-size: 30px;
+	font-family: "Overpass";
+	font-weight: 400;
+	font-style: normal;
+	line-height: 38px;
+	text-decoration: none;
+	text-transform: none;
+`,
   h3: `
-	font-size:24px;
-	font-family:"Poppins", helvetica, sans-serif;
-	font-weight:400;
-	font-style:normal;
-	text-decoration:none;
-	text-transform:none;
-	`,
+	font-size: 26px;
+	font-family: "Overpass";
+	font-weight: 400;
+	font-style: normal;
+	line-height: 33px;
+	text-decoration: none;
+	text-transform: none;
+`,
   bigParagraph: `
-	font-size:20px;
-	font-family:"Poppins", helvetica, sans-serif;
-	font-weight:400;
-	font-style:normal;
-	text-decoration:none;
-	text-transform:none;
-	`,
+	font-size: 20px;
+	font-family: "Avenir";
+	font-weight: 400;
+	font-style: normal;
+	line-height: 30px;
+	text-decoration: none;
+	text-transform: none;
+`,
+  bigParagraphSemibold: `
+	font-size: 22px;
+	font-family: "Avenir";
+	font-weight: 900;
+	font-style: normal;
+	line-height: 30px;
+	text-decoration: none;
+	text-transform: none;
+`,
   smallParagraph: `
-	font-size:16px;
-	font-family:"Poppins", helvetica, sans-serif;
-	font-weight:400;
-	font-style:normal;
-	text-decoration:none;
-	text-transform:none;
-	`,
+	font-size: 18px;
+	font-family: "Avenir";
+	font-weight: 400;
+	font-style: normal;
+	line-height: 28px;
+	text-decoration: none;
+	text-transform: none;
+`,
+  smallParagraphSemibold: `
+	font-size: 18px;
+	font-family: "Avenir";
+	font-weight: 900;
+	font-style: normal;
+	line-height: 28px;
+	text-decoration: none;
+	text-transform: none;
+`,
   extraSmallParagraph: `
-	font-size:14px;
-	font-family:"Poppins", helvetica, sans-serif;
-	font-weight:400;
-	font-style:normal;
-	text-decoration:none;
-	text-transform:none;
-	`,
+	font-size: 16px;
+	font-family: "Avenir";
+	font-weight: 400;
+	font-style: normal;
+	line-height: 21px;
+	text-decoration: none;
+	text-transform: none;
+`,
   bigMarketing: `
-	font-size:80px;
-	font-family:"Poppins", helvetica, sans-serif;
-	font-weight:400;
-	font-style:normal;
-	text-decoration:none;
-	text-transform:none;
-	`,
+	font-size: 80px;
+	font-family: "Overpass";
+	font-weight: 400;
+	font-style: normal;
+	line-height: 120px;
+	text-decoration: none;
+	text-transform: none;
+`,
   medMarketing: `
-	font-size:50px;
-	font-family:"Poppins", helvetica, sans-serif;
-	font-weight:400;
-	font-style:normal;
-	text-decoration:none;
-	text-transform:none;
-	`,
+	font-size: 50px;
+	font-family: "Poppins";
+	font-weight: 400;
+	font-style: normal;
+	text-decoration: none;
+	text-transform: none;
+`,
   gridText: `
-	font-size:14px;
-	font-family:"Poppins", helvetica, sans-serif;
-	font-weight:400;
-	font-style:normal;
-	text-decoration:none;
-  text-transform:none;
-  `,
+	font-size: 16px;
+	font-family: "Inconsolata";
+	font-weight: 400;
+	font-style: normal;
+	text-decoration: none;
+	text-transform: none;
+`,
   gridTextBold: `
-	font-size:14px;
-	font-family:"Poppins", helvetica, sans-serif;
-	font-weight:500;
-	font-style:normal;
-	text-decoration:none;
-	text-transform:none;
-	`,
+	font-size: 16px;
+	font-family: "Inconsolata";
+	font-weight: 700;
+	font-style: normal;
+	text-decoration: none;
+	text-transform: none;
+`,
 }
