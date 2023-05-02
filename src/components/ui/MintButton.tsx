77c5716815cb04d41083d328ce18f0cd1ad9bdc5
ryan-talus <rzimmerman@talusanalytics.com>
2023-05-02 17:47:08 -0300

    Updating MintButton to match new button system

--- src/components/ui/MintButton.tsx
+++ src/components/ui/MintButton.tsx
@@ -2,40 +2,91 @@ import styled, { css } from 'styled-components'
 import { lighten } from 'polished'
 import { Link } from 'gatsby'
 
-const buttonStyle = css<{ secondary?: boolean }>`
+const buttonStyle = css<{
+  secondary?: boolean
+  warning?: boolean
+  inProgress?: boolean
+}>`
   border: none;
   background: none;
   margin: 0;
   ${({ theme }) => theme.smallParagraph}
   color: ${({ theme }) => theme.black};
   border: 2px solid;
-  border-color: ${({ theme }) => theme.mint};
   padding: 10px 20px;
-
-  background-color: ${({ theme, secondary }) =>
-    secondary ? 'white' : theme.mint};
-
   transition: 150ms ease;
-
   display: flex;
   align-items: center;
   justify-content: center;
 
+  ${({ theme, secondary, warning }) => {
+    switch (true) {
+      case secondary:
+        return `
+          border-color: ${theme.mint};
+          background-color: ${theme.white};`
+      case warning:
+        return `
+          border-color: ${theme.orange};
+          background-color: ${theme.white};`
+      default:
+        return `
+          border-color: ${theme.mint};
+          background-color: ${theme.mint};`
+    }
+  }}
+
   &:hover {
-    background: ${({ theme }) => lighten(0.07, theme.mint)};
+    background: ${({ theme, secondary, warning }) => {
+      switch (true) {
+        case secondary:
+          return theme.hoverMint
+        case warning:
+          return theme.hoverOrange
+        default:
+          return theme.hoverMint
+      }
+    }};
   }
 
   &:disabled {
     background: ${({ theme }) => lighten(0.1, theme.mint)};
     color: ${({ theme }) => theme.darkGray};
+
+    ${({ theme, secondary, warning, inProgress }) => {
+      if (inProgress) {
+        switch (true) {
+          case secondary:
+            return `
+              border-color: ${theme.mint};
+              background-color: ${theme.hoverMint};`
+          case warning:
+            return `
+              border-color: ${theme.orange};
+              background-color: ${theme.hoverOrange};`
+          default:
+            return `
+              border-color: ${theme.mint};
+              background-color: ${theme.hoverMint};`
+        }
+      }
+    }}
   }
 `
 
-const MintButton = styled.button<{ secondary?: boolean }>`
+const MintButton = styled.button<{
+  secondary?: boolean
+  warning?: boolean
+  inProgress?: boolean
+}>`
   ${buttonStyle};
 `
 
-export const MintButtonLink = styled(Link)<{ secondary?: boolean }>`
+export const MintButtonLink = styled(Link)<{
+  secondary?: boolean
+  warning?: boolean
+  inProgress?: boolean
+}>`
   ${buttonStyle};
 `
 
