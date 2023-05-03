038ce8aa2d7ef2fe3c683165a236e4d3695e8cbb
ryan-talus <rzimmerman@talusanalytics.com>
2023-05-03 17:05:43 -0300

    Cleaning up unused modals

--- src/components/AuthenticatedViews/DatasetEditor/DatasetGrid/editors/TextEditor/textEditorMessages.tsx
+++ src/components/AuthenticatedViews/DatasetEditor/DatasetGrid/editors/TextEditor/textEditorMessages.tsx
@@ -4,63 +4,6 @@ import MintButton from 'components/ui/MintButton'
 import useModal from 'hooks/useModal/useModal'
 import styled from 'styled-components'
 
-interface IDMustBeUniqueProps {
-  recordIDColumn: string
-}
-
-export const IDMustBeUnique = ({ recordIDColumn }: IDMustBeUniqueProps) => {
-  const setModal = useModal()
-
-  return (
-    <div style={{ maxWidth: 500 }}>
-      <h3>{recordIDColumn} must be unique within the dataset</h3>
-      <p>
-        Unique {recordIDColumn} values are used for merging new data from
-        imported CSVs, so non-unique SampleIDs would make future updates
-        ambiguous.
-      </p>
-      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-        <MintButton onClick={() => setModal(null)}>ok</MintButton>
-      </div>
-    </div>
-  )
-}
-
-interface OnlyEditMostRecentProps {
-  latestVersion: number
-}
-
-export const OnlyEditMostRecent = ({
-  latestVersion,
-}: OnlyEditMostRecentProps) => {
-  // const datasetID = useDatasetID()
-  const setModal = useModal()
-  // const projectDispatch = useDispatch()
-
-  return (
-    <>
-      <h3 style={{}}>Only the most recent version can be edited</h3>
-      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-        <MintButton onClick={() => setModal(null)}>ok</MintButton>
-        <MintButton
-          style={{ marginLeft: 15 }}
-          secondary
-          onClick={() => {
-            console.log('reimplement SetActiveVersion', latestVersion)
-            // projectDispatch({
-            //   type: StateActions.SetActiveVersion,
-            //   payload: { datasetID, version: latestVersion },
-            // })
-            setModal(null)
-          }}
-        >
-          Go to most recent
-        </MintButton>
-      </div>
-    </>
-  )
-}
-
 const Container = styled.div`
   display: flex;
   flex-direction: column;
