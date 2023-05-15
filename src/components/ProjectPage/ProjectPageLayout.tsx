8dda736d14be17e48bdc591dd941034ef52709b3
ryan-talus <rzimmerman@talusanalytics.com>
2023-05-15 10:36:25 -0300

    Adding basic structure of the public project and dataset pages, with the fancy background

---
+++ src/components/ProjectPage/ProjectPageLayout.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const ProjectPageMain = styled.div`
+  position: relative;
+  padding: 30px;
+  color: ${({ theme }) => theme.white};
+`
--- src/components/PublicViews/ProjectPage/ProjectPage.tsx
+++ src/components/PublicViews/ProjectPage/ProjectPage.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
+
+import { ProjectPageMain } from 'components/ProjectPage/ProjectPageLayout'
+
+import PublicViewBackground from '../PublicViewBackground'
+
 import useProjectID from 'hooks/project/useProjectID'
 
 const ProjectPage = () => {
   const projectID = useProjectID()
 
-  return <p>Project Page: projectID = {projectID}</p>
+  return (
+    <>
+      <PublicViewBackground />
+      <ProjectPageMain>
+        <p>Project Page: projectID = {projectID}</p>
+      </ProjectPageMain>
+    </>
+  )
 }
 
 export default ProjectPage
