f6e6ee294eef0b8767ddedad32f931bf4d3fc340
ryan-talus <rzimmerman@talusanalytics.com>
2023-05-18 11:18:31 -0300

    Refactoring project list page to use new TopBar system; this needs a layout component

--- src/components/AuthenticatedViews/PortfolioPage/PortfolioPage.tsx
+++ src/components/AuthenticatedViews/PortfolioPage/PortfolioPage.tsx
@@ -1,21 +1,18 @@
 import React, { useState } from 'react'
-import styled from 'styled-components'
 
 import MintButton from 'components/ui/MintButton'
 import Modal from 'components/ui/Modal'
 
-import { TopBar } from '../../layout/TopBar'
 import CreateProjectForm from './CreateProjectForm/CreateProjectForm'
 import ProjectsTable from './ProjectsTable/ProjectsTable'
 import Main from 'components/layout/Main'
-import BreadcrumbLink, {
-  BreadcrumbContainer,
-} from 'components/ui/BreadcrumbLink'
 
-const H1 = styled.h1`
-  ${({ theme }) => theme.h1}
-  margin: 15px 0;
-`
+import TopBar, {
+  Title,
+  Controls,
+  Breadcrumbs,
+  BreadcrumbLink,
+} from 'components/layout/TopBar'
 
 const ProjectList = () => {
   const [createModalOpen, setCreateModalOpen] = useState(false)
@@ -23,18 +20,18 @@ const ProjectList = () => {
   return (
     <Main>
       <TopBar>
-        <BreadcrumbContainer>
+        <Breadcrumbs>
           <BreadcrumbLink to={`/projects/`}>All projects</BreadcrumbLink>
-        </BreadcrumbContainer>
-      </TopBar>
-      <TopBar>
-        <H1>My Projects</H1>
-        <MintButton onClick={() => setCreateModalOpen(true)}>
-          + New Project
-        </MintButton>
-        <Modal closeable open={createModalOpen} setOpen={setCreateModalOpen}>
-          <CreateProjectForm />
-        </Modal>
+        </Breadcrumbs>
+        <Title>My Projects</Title>
+        <Controls>
+          <MintButton onClick={() => setCreateModalOpen(true)}>
+            + New Project
+          </MintButton>
+          <Modal closeable open={createModalOpen} setOpen={setCreateModalOpen}>
+            <CreateProjectForm />
+          </Modal>
+        </Controls>
       </TopBar>
       <ProjectsTable />
     </Main>
