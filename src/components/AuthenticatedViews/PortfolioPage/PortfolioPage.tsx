30a8c92280a1d5a0b0ea843a4ecd16a822113424
ryan-talus <rzimmerman@talusanalytics.com>
2023-05-18 12:11:28 -0300

    Using margins css snippet to create layout component for the projects list page

--- src/components/AuthenticatedViews/PortfolioPage/PortfolioPage.tsx
+++ src/components/AuthenticatedViews/PortfolioPage/PortfolioPage.tsx
@@ -14,14 +14,22 @@ import TopBar, {
 
 import useModal from 'hooks/useModal/useModal'
 import styled from 'styled-components'
+import wideMargins from 'components/layout/Margins'
 
-const PortfolioPageLayout = styled.div``
+const PortfolioPageLayout = styled.div`
+  ${wideMargins}
+  display: grid;
+  grid-template-areas:
+    'topbar'
+    'projects';
+  gap: 30px;
+`
 
 const ProjectList = () => {
   const setModal = useModal()
 
   return (
-    <Main>
+    <PortfolioPageLayout>
       <TopBar>
         <Breadcrumbs>
           <BreadcrumbLink to={`/projects/`}>All projects</BreadcrumbLink>
@@ -36,7 +44,7 @@ const ProjectList = () => {
         </Controls>
       </TopBar>
       <ProjectsTable />
-    </Main>
+    </PortfolioPageLayout>
   )
 }
 export default ProjectList
