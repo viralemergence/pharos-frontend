d14f90ede06cbb46ce39cf0c500b6fa798a70c4b
Raphael Krut-Landau <raphael.kl@gmail.com>
2023-05-24 14:31:15 -0400

    Rollback changes to InputLabel
    Co-authored-by: Raphael Krut-Landau <raffi@bluetiger.digital>

--- src/components/ui/InputLabel.tsx
+++ src/components/ui/InputLabel.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 const Label = styled.label`
   ${({ theme }) => theme.extraSmallParagraph}
   display: block;
+  margin-bottom: 15px;
 `
 
 export default Label
