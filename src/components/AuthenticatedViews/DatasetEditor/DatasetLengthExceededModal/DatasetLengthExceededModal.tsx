c94044e87ea5debc555b4c687a084c069fa19608
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-06-15 18:26:50 +0000

    Limiting the max-width of the modal

--- src/components/AuthenticatedViews/DatasetEditor/DatasetLengthExceededModal/DatasetLengthExceededModal.tsx
+++ src/components/AuthenticatedViews/DatasetEditor/DatasetLengthExceededModal/DatasetLengthExceededModal.tsx
@@ -1,20 +1,25 @@
 import MintButton from 'components/ui/MintButton'
 import useModal from 'hooks/useModal/useModal'
 import React from 'react'
+import styled from 'styled-components'
 
 export const DATASET_LENGTH_LIMIT = 1000
+
+const Container = styled.div`
+  max-width: 500px;
+`
 
 const DatasetLengthExceededModal = () => {
   const setModal = useModal()
   return (
-    <div>
+    <Container>
       <h2>Dataset length limit exceeded</h2>
       <p>
         Please limit the number of rows in a dataset to {DATASET_LENGTH_LIMIT}{' '}
         or fewer. You may add as many datasets as necessary to your project.
       </p>
       <MintButton onClick={() => setModal(null)}>Ok</MintButton>
-    </div>
+    </Container>
   )
 }
 
