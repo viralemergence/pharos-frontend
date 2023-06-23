5d3835e415a2b9048920c617696fdb9b680bac3c
Raphael Krut-Landau <raphael.kl@gmail.com>
2023-06-23 12:14:44 -0400

    Checkout data.tsx from rkl/filters-singleselect
    Co-authored-by: Raphael Krut-Landau <raffi@bluetiger.digital>

--- src/pages/data.tsx
+++ src/pages/data.tsx
@@ -1,5 +1,13 @@
-import React, { useEffect } from 'react'
+import React, {
+	useEffect,
+	useRef,
+	useState,
+	Dispatch,
+	SetStateAction,
+	MutableRefObject,
+} from 'react'
 import styled from 'styled-components'
+import debounce from 'lodash/debounce'
 
 import CMS from '@talus-analytics/library.airtable-cms'
 import Providers from 'components/layout/Providers'
@@ -7,111 +15,317 @@ import Providers from 'components/layout/Providers'
 import NavBar from 'components/layout/NavBar/NavBar'
 import MapView from 'components/DataPage/MapView/MapView'
 import TableView from 'components/DataPage/TableView/TableView'
+import type { Row } from 'components/DataPage/TableView/TableView'
+import DataToolbar, { View } from 'components/DataPage/Toolbar/Toolbar'
 
-const DataViewSelectorContainer = styled.div`
-  position: absolute;
-  top: 100px;
-  left: 50%;
-  transform: translateX(-50%);
-  z-index: 10;
-`
-const DataViewSelector = styled.button<{ selected: boolean }>`
-  ${({ theme }) => theme.bigParagraph};
-  background: none;
-  border: none;
-  border: 1px solid ${({ theme }) => theme.white};
-  color: ${({ selected, theme }) => (selected ? theme.black : theme.white)};
-  min-width: 6em;
-
-  padding: 5px 5px;
-  background-color: ${({ selected, theme }) =>
-    selected ? theme.mint : theme.black};
-
-  &:first-child {
-    border-top-left-radius: 5em;
-    border-bottom-left-radius: 5em;
-  }
-
-  &:last-child {
-    border-top-right-radius: 5em;
-    border-bottom-right-radius: 5em;
-    border-left: 0px;
-  }
-`
+import FilterPanel from 'components/DataPage/FilterPanel/FilterPanel'
+import {
+	loadDebounceDelay,
+	debounceTimeout,
+	Field,
+	Filter,
+	FilterValues,
+} from 'components/DataPage/FilterPanel/constants'
 
 const ViewContainer = styled.main<{ isFilterPanelOpen: boolean }>`
-  flex: 1;
-  position: relative;
-  width: 100%;
-  display: flex;
-  flex-flow: column nowrap;
-  gap: 20px;
-  .pharos-data-toolbar {
-    flex-basis: 60px;
-    @media (max-width: 768px) {
-      ${({ isFilterPanelOpen }) => (isFilterPanelOpen ? 'display: none' : '')}
-    }
-  }
-  main {
-    display: flex;
-    flex-flow: row nowrap;
-    width: 100%;
-    height: calc(100vh - 197px);
-    @media (max-width: 768px) {
-      height: calc(100vh - 73px);
-    }
-  }
-  background-color: rgb(5, 10, 55); //#3d434e;
-  padding-bottom: 1rem;
+	flex: 1;
+	position: relative;
+	width: 100%;
+	display: flex;
+	flex-flow: column nowrap;
+	gap: 20px;
+	.pharos-data-toolbar {
+		flex-basis: 60px;
+		@media (max-width: 768px) {
+			${({ isFilterPanelOpen }) => (isFilterPanelOpen ? 'display: none' : '')}
+		}
+	}
+	main {
+		display: flex;
+		flex-flow: row nowrap;
+		width: 100%;
+		height: calc(100vh - 197px);
+		@media (max-width: 768px) {
+			height: calc(100vh - 73px);
+		}
+	}
+	background-color: rgb(5, 10, 55); //#3d434e;
+	padding-bottom: 1rem;
 `
 
-enum View {
-  table = 'table',
-  map = 'map',
+const ViewMain = styled.main`
+	position: relative;
+`
+
+const DataPage = styled.div`
+	display: flex;
+	flex-flow: column nowrap;
+	height: 100vh;
+	width: 100vw;
+`
+
+interface PublishedRecordsResponse {
+	publishedRecords: Row[]
+	isLastPage: boolean
 }
 
-const Map = (): JSX.Element => {
-  const [view, setView] = React.useState<View>(View.map)
+const isTruthyObject = (value: unknown): value is Record<string, unknown> =>
+	typeof value === 'object' && !!value
 
-  const changeView = (view: View) => {
-    window.location.hash = view
-    setView(view)
-  }
-
-  useEffect(() => {
-    const hash = window.location.hash.replace('#', '')
-
-    function hashIsView(hash: string): hash is View {
-      return Object.values(View).includes(hash)
-    }
-
-    if (hashIsView(hash)) {
-      setView(hash)
-    }
-  }, [])
-
-  return (
-    <Providers>
-      <CMS.SEO />
-      <NavBar />
-      <DataViewSelectorContainer>
-        <DataViewSelector
-          selected={view === View.map}
-          onClick={() => changeView(View.map)}
-        >
-          Map
-        </DataViewSelector>
-        <DataViewSelector
-          selected={view === View.table}
-          onClick={() => changeView(View.table)}
-        >
-          Table
-        </DataViewSelector>
-      </DataViewSelectorContainer>
-      <MapView style={{ display: view === View.map ? 'block' : 'block' }} />
-      <TableView style={{ display: view === View.table ? 'block' : 'none' }} />
-    </Providers>
-  )
+const isValidRecordsResponse = (
+	data: unknown
+): data is PublishedRecordsResponse => {
+	if (!isTruthyObject(data)) return false
+	const { publishedRecords, isLastPage } =
+		data as Partial<PublishedRecordsResponse>
+	if (!isTruthyObject(publishedRecords)) return false
+	if (typeof isLastPage !== 'boolean') return false
+	return publishedRecords.every(row => typeof row === 'object')
 }
 
-export default Map
+const isValidFieldInMetadataResponse = (data: unknown): data is Field => {
+	if (!isTruthyObject(data)) return false
+	const {
+		label,
+		dataGridKey = '',
+		type = '',
+		options = [],
+	} = data as Partial<Field>
+	if (typeof label !== 'string') return false
+	if (typeof dataGridKey !== 'string') return false
+	if (typeof type !== 'string') return false
+	if (!options.every?.(option => typeof option === 'string')) return false
+	return true
+}
+
+const isValidMetadataResponse = (data: unknown): data is MetadataResponse => {
+	if (!isTruthyObject(data)) return false
+	const { fields } = data as Partial<MetadataResponse>
+	if (!isTruthyObject(fields)) return false
+	return Object.values(fields as Record<string, unknown>).every?.(field =>
+		isValidFieldInMetadataResponse(field)
+	)
+}
+
+interface MetadataResponse {
+	fields: Record<string, Field>
+}
+
+interface Debouncing {
+	on: boolean
+	timeout: ReturnType<typeof setTimeout> | null
+}
+
+const loadPublishedRecords = async ({
+	appendResults = true,
+	filters,
+	page,
+	setLoading,
+	setPublishedRecords,
+	setAppliedFilters,
+	setReachedLastPage,
+	debouncing,
+}: {
+	appendResults?: boolean
+	filters: Filter[]
+	page: MutableRefObject<number>
+	setLoading: Dispatch<SetStateAction<boolean>>
+	setPublishedRecords: Dispatch<SetStateAction<Row[]>>
+	setAppliedFilters: Dispatch<SetStateAction<Filter[]>>
+	setReachedLastPage: Dispatch<SetStateAction<boolean>>
+	debouncing: MutableRefObject<Debouncing>
+}) => {
+	// Switch on debouncing for debounceTimeout milliseconds
+	debouncing.current.on = true
+	if (debouncing.current.timeout) clearTimeout(debouncing.current.timeout)
+	debouncing.current.timeout = setTimeout(() => {
+		debouncing.current.on = false
+	}, debounceTimeout)
+
+	if (!appendResults) page.current = 1
+	setLoading(true)
+	const params = new URLSearchParams()
+	const filtersToApply: Filter[] = []
+	for (const filter of filters) {
+		const { fieldId, values } = filter
+		values.forEach(value => {
+			params.append(fieldId, value)
+		})
+		if (values.length > 0) filtersToApply.push(filter)
+	}
+	params.append('page', page.current.toString())
+	params.append('pageSize', '50')
+	const response = await fetch(
+		`${process.env.GATSBY_API_URL}/published-records?` + params
+	)
+	if (!response.ok) {
+		console.log('GET /published-records: error')
+		setLoading(false)
+		return
+	}
+	const data = await response.json()
+	if (!isValidRecordsResponse(data)) {
+		console.log('GET /published-records: malformed response')
+		setLoading(false)
+		return
+	}
+	setPublishedRecords((previousRecords: Row[]) => [
+		...(appendResults ? previousRecords : []),
+		...data.publishedRecords,
+	])
+	setReachedLastPage(data.isLastPage)
+	setTimeout(() => {
+		setAppliedFilters(filtersToApply)
+		setLoading(false)
+	}, 0)
+}
+
+const loadPublishedRecordsDebounced = debounce(
+	loadPublishedRecords,
+	loadDebounceDelay
+)
+
+const DataView = (): JSX.Element => {
+	const [loading, setLoading] = useState(true)
+	const [publishedRecords, setPublishedRecords] = useState<Row[]>([])
+	const [reachedLastPage, setReachedLastPage] = useState(false)
+	const page = useRef(1)
+	const debouncing = useRef({ on: false, timeout: null })
+	const [view, setView] = useState<View>(View.globe)
+
+	/** Filters that will be applied to the published records */
+	const [filters, setFilters] = useState<Filter[]>([])
+
+	/** Filters that have been successfully applied to the published
+	 * records. That is, these filters have been sent to the server, and it
+	 * responded with an appropriate subset of the records. This is used for
+	 * color-coding the filtered columns. */
+	const [appliedFilters, setAppliedFilters] = useState<Filter[]>([])
+
+	const [fields, setFields] = useState<Record<string, Field>>({})
+
+	// NOTE: Setting to false to better support mobile
+	const [isFilterPanelOpen, setIsFilterPanelOpen] = useState(false)
+
+	const changeView = (view: View) => {
+		window.location.hash = view
+		setView(view)
+	}
+
+	useEffect(() => {
+		const hash = window.location.hash.replace('#', '')
+
+		function hashIsView(hash: string): hash is View {
+			return Object.values(View).includes(hash)
+		}
+
+		if (hashIsView(hash)) {
+			setView(hash)
+		}
+
+		const getMetadata = async () => {
+			const response = await fetch(
+				`${process.env.GATSBY_API_URL}/metadata-for-published-records`
+			)
+			const data = await response.json()
+			if (!isValidMetadataResponse(data)) {
+				console.error('GET /metadata-for-published-records: malformed response')
+				return
+			}
+			setFields(data.fields)
+		}
+		getMetadata()
+	}, [])
+
+	const loadFilteredRecords = (filters: Filter[], shouldDebounce = true) => {
+		const options = {
+			appendResults: false,
+			page,
+			filters,
+			setLoading,
+			setPublishedRecords,
+			setAppliedFilters,
+			setReachedLastPage,
+			debouncing,
+		}
+		if (shouldDebounce && debouncing.current.on) {
+			loadPublishedRecordsDebounced(options)
+		} else {
+			loadPublishedRecords(options)
+		}
+	}
+
+	const updateFilter = (
+		indexOfFilterToUpdate: number,
+		newValues: FilterValues
+	) => {
+		const newFilters = [...filters]
+		newFilters[indexOfFilterToUpdate].values = newValues
+		setFilters(newFilters)
+		loadFilteredRecords(newFilters)
+	}
+
+	const clearFilters = () => {
+		setFilters([])
+		if (filters.some(({ values }) => values.length > 0)) {
+			// Don't debounce when clearing filters
+			loadFilteredRecords([], false)
+		}
+	}
+
+	// TODO: Check out Ryan's PR (for project pages) to see how he does styled components
+
+	const showEarth = [View.globe, View.map].includes(view)
+
+	return (
+		<Providers>
+			<CMS.SEO />
+			<DataPage>
+				<NavBar />
+				<ViewContainer isFilterPanelOpen={isFilterPanelOpen}>
+					<DataToolbar
+						view={view}
+						changeView={changeView}
+						isFilterPanelOpen={isFilterPanelOpen}
+						setIsFilterPanelOpen={setIsFilterPanelOpen}
+						appliedFilters={appliedFilters}
+					/>
+					<MapView
+						projection={view === 'globe' ? 'globe' : 'naturalEarth'}
+						style={{
+							filter: showEarth ? 'none' : 'blur(30px)',
+						}}
+					/>
+					<ViewMain>
+						<FilterPanel
+							isFilterPanelOpen={isFilterPanelOpen}
+							setIsFilterPanelOpen={setIsFilterPanelOpen}
+							fields={fields}
+							filters={filters}
+							updateFilter={updateFilter}
+							setFilters={setFilters}
+							clearFilters={clearFilters}
+						/>
+						<TableView
+							fields={fields}
+							appliedFilters={appliedFilters}
+							loadPublishedRecords={() => {
+								loadFilteredRecords(filters, false)
+								debouncing.current.on = false
+							}}
+							loading={loading}
+							page={page}
+							publishedRecords={publishedRecords}
+							reachedLastPage={reachedLastPage}
+							style={{
+								display: view === View.table ? 'grid' : 'none',
+							}}
+						/>
+					</ViewMain>
+				</ViewContainer>
+			</DataPage>
+		</Providers>
+	)
+}
+
+export default DataView
