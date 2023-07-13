df72a97424eab530c636c9d6b879871ac0117b92
Ryan Zimmerman <61983809+ryan-talus@users.noreply.github.com>
2023-07-13 20:45:44 +0100

    Adding publicProjectPageContentBox to match project page pattern and design

--- src/components/PublicViews/ProjectPage/ProjectPage.tsx
+++ src/components/PublicViews/ProjectPage/ProjectPage.tsx
@@ -13,6 +13,15 @@ import TopBar, {
 
 import usePublishedProject from './usePublishedProject'
 
+const PublicProjectPageContentBox = styled(ProjectPageContentBox)`
+  background-color: ${({theme}) => theme.medBlack};
+  border-top: 5px solid ${({theme}) => theme.mint};
+
+  > h2 {
+    color: ${({theme}) => theme.medDarkGray};
+  }
+`
+
 const ProjectPage = () => {
   const project = usePublishedProject()
 
