d213115ee6aa43ebe8e84a612ae05d39af01565e
Raphael Krut-Landau <raphael.kl@gmail.com>
2023-07-17 11:48:51 -0400

    Fixing yarn tsc errors
    Co-authored-by: Raphael Krut-Landau <raffi@bluetiger.digital>

--- src/components/DataPage/TableView/TableView.tsx
+++ src/components/DataPage/TableView/TableView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 import DataGrid, { Column } from 'react-data-grid'
