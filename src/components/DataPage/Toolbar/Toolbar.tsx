152a80166e4d468315ab96742273e710a204ecc8
Raphael Krut-Landau <raphael.kl@gmail.com>
2023-07-20 16:54:06 -0400

    Correct padding and spacing of data page toolbar
    Co-authored-by: Raphael Krut-Landau <raffi@bluetiger.digital>

--- src/components/DataPage/Toolbar/Toolbar.tsx
+++ src/components/DataPage/Toolbar/Toolbar.tsx
@@ -26,8 +26,7 @@ const DataToolbarButton = styled.button<{
       &:hover { background-color: ${theme.white10PercentOpacity}; }
       `}
   color: ${({ selected, theme }) => (selected ? theme.black : theme.white)};
-  border-radius: 5px;
-  margin-right: 5px;
+  border-radius: 7px;
   cursor: pointer;
   &:last-child {
     margin-right: 0;
@@ -43,13 +42,17 @@ const DataToolbarFiltersButton = styled(DataToolbarButton)`
   padding-left: 10px;
   padding-right: 10px;
   margin-left: 0;
+  border-radius: 9px;
 `
 
 const DataToolbarRadioButton = styled(DataToolbarButton)``
 const DataToolbarButtonContainer = styled.div`
   background-color: ${({ theme }) => theme.white20PercentOpacity};
-  border-radius: 5px;
+  border-radius: 10px;
   position: relative;
+  display: flex;
+  flex-flow: row nowrap;
+  gap: 10px;
   backdrop-filter: blur(3px);
   border: 1px solid ${({ theme }) => theme.white10PercentOpacity};
   box-shadow: 0 0 30px rgba(0, 0, 0, 0.124);
@@ -57,12 +60,16 @@ const DataToolbarButtonContainer = styled.div`
 const DataToolbarRadioButtonContainer = styled(DataToolbarButtonContainer)`
   padding: 5px;
 `
+const DataToolbarFilterButtonContainer = styled(DataToolbarButtonContainer)`
+  height: 42px;
+`
 const DataToolbarDiv = styled.div<{ isFilterPanelOpen: boolean }>`
   padding: 20px 0 0 30px;
   display: flex;
   flex-flow: row wrap;
   gap: 10px;
   flex-basis: 60px;
+  align-items: center;
   @media (max-width: ${({ theme }) => theme.breakpoints.tabletMaxWidth}) {
     padding: 10px;
     ${({ isFilterPanelOpen }) => (isFilterPanelOpen ? 'display: none' : '')}
@@ -97,7 +104,7 @@ const DataToolbar = ({
 
   return (
     <DataToolbarDiv isFilterPanelOpen={isFilterPanelOpen}>
-      <DataToolbarButtonContainer>
+      <DataToolbarFilterButtonContainer>
         <DataToolbarFiltersButton
           selected={isFilterPanelOpen}
           onClick={() => {
@@ -107,7 +114,7 @@ const DataToolbar = ({
         >
           Filters
         </DataToolbarFiltersButton>
-      </DataToolbarButtonContainer>
+      </DataToolbarFilterButtonContainer>
       <DataToolbarRadioButtonContainer>
         <ViewRadioButton forView={View.map} label="Map" />
         <ViewRadioButton forView={View.globe} label="Globe" />
