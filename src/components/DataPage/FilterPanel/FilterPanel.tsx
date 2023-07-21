c38cd4dc771a610e1e685765aaa3acc8b9d75820
Raphael Krut-Landau <raphael.kl@gmail.com>
2023-07-20 23:30:20 -0400

    Remove unneeded calc()
    Co-authored-by: Raphael Krut-Landau <raffi@bluetiger.digital>

--- src/components/DataPage/FilterPanel/FilterPanel.tsx
+++ src/components/DataPage/FilterPanel/FilterPanel.tsx
@@ -152,9 +152,6 @@ const FilterList = styled.ul`
   overflow-y: auto;
   padding: 34px 40px;
   flex: 1;
-  @media (max-width: ${({ theme }) => theme.breakpoints.tabletMaxWidth}) {
-    height: calc(100vh - 60px - 73px);
-  }
   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMaxWidth}) {
     padding: 34px 20px;
   }
