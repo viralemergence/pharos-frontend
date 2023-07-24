7943ba8ff13e86f100cea39045801374536ccae2
Raphael Krut-Landau <raphael.kl@gmail.com>
2023-07-24 12:47:43 -0400

    Fix tsc error
    Co-authored-by: Raphael Krut-Landau <raffi@bluetiger.digital>

--- src/pages/data.spec.tsx
+++ src/pages/data.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 import { stateInitialValue } from 'reducers/stateReducer/initialValues'
 import { publishedRecordsMetadata } from '../../test/serverHandlers'
