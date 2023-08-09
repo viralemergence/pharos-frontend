5f5f9fda641d7374852b03c81222d2e73f83c183
ryan-talus <rzimmerman@talusanalytics.com>
2023-08-09 20:25:08 -0300

    Clean up extra pushstate / popstate calls

--- src/components/PublicViews/Researchers/ResearchersListPage/useResearchersListFilters.ts
+++ src/components/PublicViews/Researchers/ResearchersListPage/useResearchersListFilters.ts
@@ -20,27 +20,47 @@ const getFiltersFromParams = () => {
   return paramsFilters
 }
 
-const setFiltersInParams = (filters: PublishedResearchersFilters) => {
-  if (typeof window === 'undefined') return
-
+const convertFiltersToParams = (filters: PublishedResearchersFilters) => {
   const params = new URLSearchParams()
   Object.entries(filters).forEach(([key, value]) => {
     if (value) params.append(key, value)
   })
+  return params
+}
+
+const setFiltersInParams = (filters: PublishedResearchersFilters) => {
+  if (typeof window === 'undefined') return
+
+  const params = convertFiltersToParams(filters)
 
   let nextURL = `${window.location.pathname}`
+
+  // add search params to url only if there are filters
   if (params.toString() !== '') nextURL += `?${params.toString()}`
+
+  // don't replace or push state if the url is the same
+  if (nextURL === `${window.location.pathname}${window.location.search}`) return
 
   // replace state if the user is typing a search
   if (filters.searchString) window.history.replaceState({}, '', nextURL)
-  // for other filters use pushState so they can "go back"
+  // for all other filters use pushState so they can "go back"
   else window.history.pushState({}, '', nextURL)
 }
 
 const handlePopState = (
   setFilters: React.Dispatch<React.SetStateAction<PublishedResearchersFilters>>
 ) => {
-  setFilters(getFiltersFromParams())
+  if (typeof window === 'undefined') return
+
+  setFilters(prev => {
+    const params = new URLSearchParams(window.location.search)
+    const filterParams = convertFiltersToParams(prev)
+
+    if (params.toString() === filterParams.toString()) return prev
+
+    console.log('popstate set filters')
+    return getFiltersFromParams()
+  })
 }
 
 // handle filters in state and synchronize with url search params
@@ -49,6 +69,7 @@ const usePublishedResearchersFilters = () => {
     getFiltersFromParams()
   )
 
+  // update our filters on popstate
   useEffect(() => {
     window.addEventListener('popstate', () => handlePopState(setFilters))
     return () => window.removeEventListener('popstate', () => handlePopState)
