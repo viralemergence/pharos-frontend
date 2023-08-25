7fef312a3d91b8627834db31914ccde0c2b9f9d9
Raphael Krut-Landau <raphael.kl@gmail.com>
2023-08-25 12:21:42 -0400

    Add test
    Co-authored-by: Raphael Krut-Landau <raffi@bluetiger.digital>

--- src/components/DataPage/FilterPanel/components.tsx
+++ src/components/DataPage/FilterPanel/components.tsx
@@ -1,10 +1,4 @@
-import React, {
-  Dispatch,
-  SetStateAction,
-  useEffect,
-  useMemo,
-  useState,
-} from 'react'
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import debounce from 'lodash/debounce'
 import type { Filter } from 'pages/data'
 import {
