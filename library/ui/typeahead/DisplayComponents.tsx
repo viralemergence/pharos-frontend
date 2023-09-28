06f9c86d519f1a3575ddb98f0b3d12e2190e8109
ryan-talus <rzimmerman@talusanalytics.com>
2023-09-28 10:01:25 -0300

    making disabled typeaheads look disabled on all browsers

--- library/ui/typeahead/DisplayComponents.tsx
+++ library/ui/typeahead/DisplayComponents.tsx
@@ -78,6 +78,11 @@ export const SearchBar = styled.input<{
             opacity: 0.66
           `};
   }
+
+  &:disabled {
+    opacity: 0.5;
+    border: 1px solid #eee !important;
+  }
 `
 export const Results = styled.div<{ resultsMaxHeight: string }>`
   display: flex;
