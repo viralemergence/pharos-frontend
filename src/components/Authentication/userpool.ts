1b859bb20bc197caf1d663c0c4042d7ff4a4192b
ryan-talus <rzimmerman@talusanalytics.com>
2023-10-05 18:07:49 -0300

    Fixing user pool id and client ID everywhere

--- src/components/Authentication/userpool.ts
+++ src/components/Authentication/userpool.ts
@@ -1,10 +1,8 @@
 import { CognitoUserPool } from 'amazon-cognito-identity-js'
 
 const poolData = {
-  UserPoolId: 'us-east-2_IfMtrhdEW',
-  ClientId: '5m5c0obcj34d5jd06oufjh5omb',
-  // UserPoolId: process.env.GATSBY_USER_POOL_ID,
-  // ClientId: process.env.GATSBY_CLIENT_ID,
+  UserPoolId: process.env.GATSBY_USER_POOL_ID!,
+  ClientId: process.env.GATSBY_CLIENT_ID!,
 }
 
 export default new CognitoUserPool(poolData)
