c2b13b968b1abafe93fea988e73ed9feead1084e
ryan-talus <rzimmerman@talusanalytics.com>
2023-11-13 12:15:14 -0300

    Adding google analytics ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
       // the cookieconsent opt-in system.
       resolve: `gatsby-plugin-gtag`,
       options: {
-        trackingId: `G-XXXXXXXXXX`,
+        trackingId: `G-RZHBVVJL8K`,
         anonymize: true,
         head: false,
       },
