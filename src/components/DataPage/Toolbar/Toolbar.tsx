299933b241da46f3a6e9902c9c7f031f6f9df82f
ryan-talus <rzimmerman@talusanalytics.com>
2023-11-14 11:59:16 -0300

    Temporarily taking download button off of the data page

--- src/components/DataPage/Toolbar/Toolbar.tsx
+++ src/components/DataPage/Toolbar/Toolbar.tsx
@@ -12,10 +12,11 @@ import {
   ContainerForRadioButtons,
   ContainerForFilterPanelLauncher,
   DataToolbarDiv,
+  SummaryOfRecordsStyled,
   // SummaryOfRecordsStyled,
 } from '../DisplayComponents'
 import { SummaryOfRecords } from 'components/PublicViews/PublishedRecordsDataGrid/PublishedRecordsDataGrid'
-import DataDownloadButton from './DataDownloadButton'
+// import DataDownloadButton from './DataDownloadButton'
 
 export enum View {
   map = 'map',
@@ -28,7 +29,7 @@ export const isView = (str: string): str is View => {
 }
 
 // /** For example, convert 1000000 to "1,000,000" */
-// const addCommasToNumber = (num: number) => num.toLocaleString('en-US')
+const addCommasToNumber = (num: number) => num.toLocaleString('en-US')
 
 const RadioButton = ({
   currentView,
@@ -126,21 +127,23 @@ const DataToolbar = ({
       )}
       {view === View.table && recordCount !== undefined && (
         <>
-          <DataDownloadButton
-            matchingRecordCount={matchingRecordCount}
-            filters={filters}
-          />
           {
-            // <SummaryOfRecordsStyled
-            //   role="status"
-            //   aria-live="polite"
-            //   aria-atomic="true"
-            // >
-            //   {matchingRecordCount !== undefined && appliedFiltersCount > 0 && (
-            //     <>{addCommasToNumber(matchingRecordCount)} of </>
-            //   )}
-            //   {addCommasToNumber(recordCount)} records
-            // </SummaryOfRecordsStyled>
+            // <DataDownloadButton
+            //   matchingRecordCount={matchingRecordCount}
+            //   filters={filters}
+            // />
+          }
+          {
+            <SummaryOfRecordsStyled
+              role="status"
+              aria-live="polite"
+              aria-atomic="true"
+            >
+              {matchingRecordCount !== undefined && appliedFiltersCount > 0 && (
+                <>{addCommasToNumber(matchingRecordCount)} of </>
+              )}
+              {addCommasToNumber(recordCount)} records
+            </SummaryOfRecordsStyled>
           }
         </>
       )}
