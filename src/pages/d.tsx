0000d241de52abf3c70c6d18724dee04983d17c2
ryan-talus <rzimmerman@talusanalytics.com>
2023-11-20 20:10:26 -0300

    Adding formatting for "applied filters" on data download page

--- src/pages/d.tsx
+++ src/pages/d.tsx
@@ -16,6 +16,7 @@ import ClickToCopy from 'components/ui/ClickToCopy'
 import { MintButtonLink } from 'components/ui/MintButton'
 import { Link, navigate } from 'gatsby'
 import ModalMessageProvider from 'hooks/useModal/ModalMessageProvider'
+import { lighten, saturate } from 'polished'
 import React from 'react'
 import styled from 'styled-components'
 
@@ -44,6 +45,109 @@ const DownloadPageContentBox = styled(PublicProjectPageContentBox)`
 
   a {
     color: ${({ theme }) => theme.mint};
+  }
+`
+
+const AppliedFilters = styled.div`
+  margin-top: 10px;
+  color: ${({ theme }) => theme.white};
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  background: ${({ theme }) => theme.mutedPurple1};
+  border: 1px solid
+    ${({ theme }) => saturate(0.1, lighten(-0.08, theme.mutedPurple1))};
+  padding: 10px;
+  border-radius: 10px;
+`
+
+const Filter = styled.div`
+  background: ${({ theme }) => lighten(-0.02, theme.mutedPurple2)};
+  border: 1px solid
+    ${({ theme }) => saturate(0.1, lighten(-0.11, theme.mutedPurple2))};
+  ${({ theme }) => theme.smallParagraphSemibold};
+  display: flex;
+  flex-direction: column;
+  padding: 5px 13px 10px 13px;
+  border-radius: 5px;
+
+  &:before {
+    ${({ theme }) => theme.smallParagraph};
+    color: ${({ theme }) => lighten(0.1, saturate(-0.03, theme.mutedPurple4))};
+    font-style: italic;
+    content: 'AND';
+    font-size: 0.9em;
+  }
+
+  &:first-of-type:before {
+    content: '';
+    display: none;
+  }
+`
+
+const Label = styled.span`
+  margin-bottom: 10px;
+`
+
+const Value = styled.div`
+  position: relative;
+  border-left: 1px solid ${({ theme }) => theme.mutedPurple3};
+  margin-left: 5em;
+  padding: 0px 8px 3px 8px;
+  display: flex;
+  align-items: baseline;
+
+  &:before {
+    content: 'OR';
+    position: absolute;
+    left: -9px;
+    transform: translateX(-100%);
+    color: ${({ theme }) => lighten(0.1, saturate(-0.03, theme.mutedPurple4))};
+    ${({ theme }) => theme.smallParagraph};
+    font-size: 0.9em;
+    font-style: italic;
+  }
+
+  &:first-of-type {
+    // margin-top: 5px;
+  }
+
+  &:first-of-type:before {
+    content: 'MATCH';
+  }
+`
+
+const DateFilter = styled(Filter)``
+
+const DateRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  // justify-content: space-between;
+  gap: 3em;
+  @media (max-width: 1300px) {
+    justify-content: flex-start;
+    gap: 10px;
+  }
+`
+
+const DateValue = styled(Value)`
+  flex-direction: column;
+  border-left: none;
+  margin-left: 0;
+  margin-top: 1em;
+  padding-left: 0;
+  padding-right: 0;
+
+  &:before {
+    top: 3px;
+    left: unset;
+    bottom: unset;
+    transform: translateY(-100%);
+    content: 'TO';
+  }
+
+  &:first-of-type:before {
+    content: 'FROM';
   }
 `
 
@@ -168,7 +272,42 @@ const Downloads = () => {
                 <ProjectPageSidebar>
                   <DownloadPageContentBox>
                     <h2>Applied Filters</h2>
-                    <pre>{JSON.stringify(appliedFilters, null, 2)}</pre>
+                    <AppliedFilters>
+                      {(appliedFilters.collection_start_date ||
+                        appliedFilters.collection_end_date) && (
+                        <DateFilter>
+                          <Label>Collection date</Label>
+                          <DateRow>
+                            <DateValue>
+                              {appliedFilters.collection_start_date || 'Any'}
+                            </DateValue>
+                            <DateValue>
+                              {appliedFilters.collection_end_date || 'Any'}
+                            </DateValue>
+                          </DateRow>
+                        </DateFilter>
+                      )}
+                      {Object.entries(appliedFilters)
+                        .filter(
+                          ([key, _]) =>
+                            ![
+                              'collection_start_date',
+                              'collection_end_date',
+                            ].includes(key)
+                        )
+                        .map(([filter, values]) => (
+                          <Filter>
+                            <Label>{filter}</Label>
+                            {values.map(value => (
+                              <Value>{value}</Value>
+                            ))}
+                            <Value>{'Other value'}</Value>
+                          </Filter>
+                        ))}
+                    </AppliedFilters>
+                    {
+                      // <pre>{JSON.stringify(appliedFilters, null, 2)}</pre>
+                    }
                   </DownloadPageContentBox>
                 </ProjectPageSidebar>
               </>
