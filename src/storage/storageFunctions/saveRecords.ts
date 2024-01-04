fbebf7733d88968a698de32b5e7df5a6a669ce9a
ryan-talus <rzimmerman@talusanalytics.com>
2024-01-04 12:35:02 -0300

    Add throttling to the save-records route

--- src/storage/storageFunctions/saveRecords.ts
+++ src/storage/storageFunctions/saveRecords.ts
@@ -18,6 +18,48 @@ export type SaveRecords = StorageMessagePayload<
   }
 >
 
+const throttleAsync = (fn: (...args: unknown[]) => Promise<Response>, wait: number) => {
+  let lastRun = 0;
+
+  const throttled = async (...args: unknown[]): Promise<Response> => {
+    const currentWait = lastRun + wait - Date.now();
+    const shouldRun = currentWait <= 0;
+
+    if (shouldRun) {
+      lastRun = Date.now();
+
+      return await fn(...args);
+    } else {
+      return await new Promise(function(resolve) {
+        setTimeout(function() {
+          resolve(throttled(...args));
+        }, currentWait);
+      });
+    }
+  }
+
+  return throttled;
+}
+
+
+const requestSaveRecords = async (userSession: Awaited<ReturnType<typeof Auth.currentSession>>, message: SaveRecords) => {
+  const response = await fetch(
+    `${process.env.GATSBY_API_URL}/${message.route}`,
+    {
+      method: 'POST',
+      headers: new Headers({
+        Authorization: userSession.getIdToken().getJwtToken(),
+        'Content-Type': 'application/json',
+      }),
+      body: JSON.stringify({ ...message.data }),
+    }
+  )
+
+  return response
+}
+
+const throttledRequestSaveRecords = throttleAsync(requestSaveRecords, 500)
+
 const saveRecords: StorageFunction<SaveRecords> = async (
   key,
   message,
@@ -57,17 +99,7 @@ const saveRecords: StorageFunction<SaveRecords> = async (
 
   let response
   try {
-    response = await fetch(
-      `${process.env.GATSBY_API_URL}/${message.route}`,
-      {
-        method: 'POST',
-        headers: new Headers({
-          Authorization: userSession.getIdToken().getJwtToken(),
-          'Content-Type': 'application/json',
-        }),
-        body: JSON.stringify({ ...message.data }),
-      }
-    )
+    response = await throttledRequestSaveRecords(userSession, message)
   } catch (e) {
     if (!navigator.onLine)
       dispatch({
